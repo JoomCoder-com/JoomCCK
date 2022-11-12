4b296f50df120bcc1a79939e50310c2eb498099b
ANDI Amine <andi.amine@gmail.com>
2022-11-12 14:30:30 +0100

    Fixed - Category limit field was not working correctly

--- components/com_joomcck/library/php/html/mrelements/catselector.php
+++ components/com_joomcck/library/php/html/mrelements/catselector.php
@@ -14,15 +14,14 @@ defined('_JEXEC') or die('Restricted access');
 	<br />
 <?php endif;?>
 <div id="error-msg"></div>
-<div id="categories_list">
+<div id="categories_list" class="clearfix">
 	<?php if (!empty($defaults)): ?>
 		<?php	foreach ( $defaults as $item ):?>
-		<div class="float-start alert alert-info">
-			<button type="button" class="close" onclick="CatSelector.remove(<?php echo $item->id;?>)" data-dismiss="alert">×</button>
-			<?php echo $item->title;?>
-			<br /><small><?php echo $item->path;?></small>
-			<input type="hidden" name="<?php echo $name?>" value="<?php echo $item->id;?>" />
-		</div>
+            <div class="float-start alert alert-success alert-dismissible fade show" role="alert">
+                <strong><?php echo $item->title;?></strong> <?php echo $item->path;?>
+                <button type="button" class="btn-close" onclick="CatSelector.remove(<?php echo $item->id;?>)" data-bs-dismiss="alert" aria-label="Close"></button>
+                <input type="hidden" name="<?php echo $name?>" value="<?php echo $item->id;?>" />
+            </div>
 		<?php endforeach;?>
 	<?php endif; ?>
 </div>
@@ -34,7 +33,7 @@ defined('_JEXEC') or die('Restricted access');
 				<ul id="level1" class="nav nav-list">
 					<?php foreach ($sections AS $s):?>
 						<li id="category-<?php echo $s->id; ?>-1">
-							<a href="javascript:void(0);" onclick="CatSelector.get_children(1, <?php echo $s->id?>, 1)"><?php echo $s->name?></a>
+							<a class="btn btn-outline-dark" href="javascript:void(0);" onclick="CatSelector.get_children(1, <?php echo $s->id?>, 1)"><?php echo $s->name?></a>
 						</li>
 					<?php endforeach;?>
 				</ul>
@@ -47,7 +46,7 @@ defined('_JEXEC') or die('Restricted access');
 					<?php foreach ($categories AS $c):?>
 						<?php if(in_array($c->id, $ignore)) { continue;} ?>
 						<li id="category-<?php echo $c->section_id; ?>-<?php echo $c->id; ?>">
-							<a href="javascript:void(0);" <?php if($c->children){ echo 'onclick="CatSelector.get_children('.$c->id.', '.$c->section_id.', 1)"';}else{echo ' class="disabled"';}?>>
+							<a class="btn btn-outline-dark" href="javascript:void(0);" <?php if($c->children){ echo 'onclick="CatSelector.get_children('.$c->id.', '.$c->section_id.', 1)"';}else{echo ' class="disabled"';}?>>
 								<?php if($c->id != JFactory::getApplication()->input->getInt('id') && $c->params->get('submission', 1)):?>
 									<img class="float-end" id="s_icon<?php echo $c->id;?>"
 										src="<?php echo JURI::root(TRUE);?>/media/mint/icons/16/plus-button.png"
@@ -91,19 +90,23 @@ defined('_JEXEC') or die('Restricted access');
 			<?php if($limit > 0):?>
 				if(this.s_icons_cat.length >= <?php echo $limit;?>)
 				{
-					var html = '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">×</button><?php echo htmlentities(JText::_('CCATEGORYREACHMAXLIMIT'), ENT_QUOTES, 'UTF-8')?></div>';
+					var html = '<div class="float-start alert alert-warning alert-dismissible fade show" role="alert">' +
+                        '<button type="button" data-bs-dismiss="alert" aria-label="Close" class="btn-close">' +
+                        '</button>
+                        <?php echo htmlentities(JText::_('CCATEGORYREACHMAXLIMIT'), ENT_QUOTES, 'UTF-8')?>
+                        </div>';
 					$('#error-msg').html(html);
 					return;
 				}
 			<?php endif;?>
 			this.s_icons_cat.push(id);
 
-			var out = '<button type="button" class="close" onclick="CatSelector.remove(' + id +
-			')" data-dismiss="alert">×</button>' + title + '<br /><small>'+ path +'</small>' +
-			'<input type="hidden" name="<?php echo $name?>" value="' + id + '" />';
+			var out = '<strong>' + title + '</strong> ' + path +
+                '<button type="button"  data-bs-dismiss="alert" aria-label="Close" class="btn-close" onclick="CatSelector.remove(' + id +
+			')" ></button><input type="hidden" name="<?php echo $name?>" value="' + id + '" />';
 
 			var li = $(document.createElement("div"))
-				.attr('class', 'float-start alert alert-info')
+				.attr('class', 'float-start alert alert-success alert-dismissible fade show')
 				.html(out);
 			$('#categories_list').append(li);
 			$('#s_icon' + id).css('display', 'none');
@@ -112,8 +115,15 @@ defined('_JEXEC') or die('Restricted access');
 		remove: function(id)
 		{
 			$('#error-msg').html('');
-			this.s_icons_cat.erase(id);
-			try{$('#s_icon' + id).css('display', 'block');}catch(e){}
+			this.s_icons_cat = this.s_icons_cat.filter(function(e) { return e !== 'seven' });
+			try{
+                $('#s_icon' + id).css('display', 'block');
+                $('#selectedCat'+id).remove();
+
+
+            }catch(e){
+
+            }
 		},
 
 		icons_hide: function()
@@ -181,7 +191,7 @@ defined('_JEXEC') or die('Restricted access');
 						var li = $(document.createElement('li'))
 							.attr('id', 'category-'+ section + '-' + obj.id);
 						var html = '';
-						html += '<a href="javascript: void(0);"';
+						html += '<a class="btn btn-outline-dark d-flex justify-content-between" href="javascript: void(0);"';
 						if(obj.children > 0)
 						{
 							html += 'onclick="CatSelector.get_children(' + obj.id +', ' + obj.section_id +', '+ (level + 1)+')"';
@@ -194,7 +204,7 @@ defined('_JEXEC') or die('Restricted access');
 
 						if(obj.params.submission == "1")
 						{
-							html += '<img class="float-end" id="s_icon' + obj.id +
+							html += '<img id="s_icon' + obj.id +
 								'" src="<?php echo JURI::root(TRUE);?>/media/mint/icons/16/plus-button.png" ' +
 								'onclick="CatSelector.add(' + obj.id +', \'' + obj.title +'\', \'' + obj.path +'\')" />';
 						}
@@ -216,7 +226,9 @@ defined('_JEXEC') or die('Restricted access');
 
 				var list = $('#level'+level).children();
 				$.each(list, function(item, object){
-					object.removeClass('active');
+
+
+					$(object).removeClass('active');
 				});
 
 			    $('#category-'+ section + '-' + id).addClass('active');
