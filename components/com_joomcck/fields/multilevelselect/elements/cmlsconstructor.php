77ff289cb78aadc1d7a5e7eed0c74852f44d15c3
ANDI Amine <andi.amine@gmail.com>
2022-12-02 03:43:42 +0100

    Fixed - Values constructor modals was not working [Multilevelselct field]

--- components/com_joomcck/fields/multilevelselect/elements/cmlsconstructor.php
+++ components/com_joomcck/fields/multilevelselect/elements/cmlsconstructor.php
@@ -6,6 +6,10 @@
  * @copyright Copyright (C) 2012 JoomBoost (https://www.joomBoost.com). All rights reserved.
  * @license GNU/GPL http://www.gnu.org/copyleft/gpl.html
  */
+
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
+
 defined('_JEXEC') or die();
 
 jimport('joomla.form.formfield');
@@ -19,24 +23,76 @@ class JFormFieldCmlsconstructor extends JFormField
 		$app = JFactory::getApplication();
 		if($fid = $app->input->getInt('id'))
 		{
-			$url_form = JURI::root(TRUE).'/index.php?option=com_joomcck&view=elements&layout=field&id=' . $fid . '&func=_getConstructor&record=0&section_id=0&tmpl=component&width=640';
 
-			$out = '&nbsp;<a rel="{handler: \'iframe\', size: {x: 550, y: 500} }"
-	    			onclick="return false;"
-	    			href="'.$url_form.'"
-	    			class="cmodal btn btn-primary">'.JText::_('MLS_CONSTRUCT').'</a>';
+			// edit data modal and button
+			$editData = [
+				'type' => 'editData',
+				'attrId' => 'editDataModal'.$fid,
+				'fieldId' => $fid,
+				'buttonIcon' => 'fas fa-edit',
+				'buttonText' => JText::_('MLS_CONSTRUCT'),
+				'iframeUrl' => JURI::root(TRUE).'/index.php?option=com_joomcck&view=elements&layout=field&id=' . $fid . '&func=_getConstructor&record=0&section_id=0&tmpl=component&width=640'
+			];
 
-			$url_form = JURI::root(TRUE).'/index.php?option=com_joomcck&view=elements&layout=field&id=' . $fid . '&func=_getLoader&record=0&section_id=0&tmpl=component&width=640';
-			$out .= '&nbsp;<a rel="{handler: \'iframe\', size: {x: 550, y: 500} }"
-	    			onclick="return false;"
-	    			href="'.$url_form.'"
-	    			class="cmodal btn">'.JText::_('MLS_LOAD').'</a>';
+			$out = $this->getModal($editData);
+
+
+			// edit data modal and button
+			$loadData = [
+				'type' => 'loadData',
+				'attrId' => 'loadDataModal'.$fid,
+				'fieldId' => $fid,
+				'buttonIcon' => 'fas fa-plus',
+				'buttonText' => JText::_('MLS_LOAD'),
+				'iframeUrl' => JURI::root(TRUE).'/index.php?option=com_joomcck&view=elements&layout=field&id=' . $fid . '&func=_getLoader&record=0&section_id=0&tmpl=component&width=640'
+			];
+
+			$out .= $this->getModal($loadData);
 
 			return $out;
 		}
 		else
-		return JText::_('Please save field to set values');
+			return JText::_('Please save field to set values');
 
 	}
+
+
+	protected function getModal($modalData){
+
+		$target = $modalData['type'].$modalData['fieldId'] . 'Modal';
+
+
+		// modal button
+		$html = '<button'
+			. ' type="button"'
+			. ' id="' . $modalData['attrId'] . '"'
+			. ' class="btn btn-secondary"'
+			. ' data-bs-toggle="modal"'
+			. ' data-bs-target="#'.$target.'">'
+			. '<span class="'.$modalData['buttonIcon'].'" aria-hidden="true"></span> '
+			. $modalData['buttonText']
+			. '</button>';
+
+		// build modal
+		$html .= HTMLHelper::_(
+			'bootstrap.renderModal',
+			$target,
+			array(
+				'title'       => $modalData['buttonText'],
+				'backdrop'    => 'static',
+				'url'         => $modalData['iframeUrl'],
+				'height'      => '400px',
+				'width'       => '800px',
+				'bodyHeight'  => 70,
+				'modalWidth'  => 80,
+				'footer'      => '<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">'
+					. Text::_('JLIB_HTML_BEHAVIOR_CLOSE') . '</button>',
+			)
+		);
+
+		return $html;
+
+
+	}
+
 }
-?>
