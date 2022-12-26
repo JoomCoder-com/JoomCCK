67b399e6c2e68e1cc99805f8edd17164f8d3fd16
ANDI Amine <andi.amine@gmail.com>
2022-12-26 04:40:07 +0100

    Improved - before/after input group BS5 [Digits Field]

--- components/com_joomcck/fields/digits/tmpl/input/default.php
+++ components/com_joomcck/fields/digits/tmpl/input/default.php
@@ -23,15 +23,14 @@ $required = $required;
 $prep = $this->params->get('params.prepend', NULL);
 $app = $this->params->get('params.append', NULL)
 ?>
-<div class="<?php if($prep) {echo ' input-prepend';} if($app) {echo ' input-append';}?>">
+<div class="input-group">
 	<?php if($prep):?>
-		<span class="add-on"><?php echo $prep;?></span>
+		<span class="input-group-text"><?php echo $prep;?></span>
 	<?php endif; ?>
 	<input type="text" name="jform[fields][<?php echo $this->id;?>]" id="field_<?php echo $this->id;?>" value="<?php echo $this->value;?>"
 		size="<?php echo $this->params->get('params.field_size', 10);?>"
-		onKeyUp="Joomcck.fieldErrorClear(<?php echo $this->id; ?>); Joomcck.formatFloat(this, <?php echo $this->params->get('params.decimals_num', 0);?>, <?php echo $this->params->get('params.max_num', false);?>, <?php echo $this->params->get('params.val_max', false);?>,	<?php echo $this->params->get('params.val_min', 0);?>, <?php echo $this->id;?>, '<?php echo JText::sprintf('D_MINMAX_ERROR', $this->label, $this->params->get('params.val_min', 0), $this->params->get('params.val_max', 0), array('jsSafe' => true));?>');"
-		<?php echo $class.$required;?> />
+		onKeyUp="Joomcck.fieldErrorClear(<?php echo $this->id; ?>); Joomcck.formatFloat(this, <?php echo $this->params->get('params.decimals_num', 0);?>, <?php echo $this->params->get('params.max_num', false);?>, <?php echo $this->params->get('params.val_max', false);?>,	<?php echo $this->params->get('params.val_min', 0);?>, <?php echo $this->id;?>, '<?php echo JText::sprintf('D_MINMAX_ERROR', $this->label, $this->params->get('params.val_min', 0), $this->params->get('params.val_max', 0), array('jsSafe' => true));?>');" <?php echo $class.$required;?>/>
 	<?php if($app):?>
-		<span class="add-on"><?php echo $app;?></span>
+		<span class="input-group-text"><?php echo $app;?></span>
 	<?php endif; ?>
 </div>
