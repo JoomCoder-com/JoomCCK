d61914f1eff41b5a9ce5433994ba360e7abe15f0
ANDI Amine <andi.amine@gmail.com>
2023-05-12 23:45:12 +0100

    Improved - tags field now use TomSelect JS lib

---
+++ components/com_joomcck/layouts/core/fields/tomSelectTagsAjax.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * by joomcoder
+ * a component for Joomla! 3.x CMS (http://www.joomla.org)
+ * Author Website: https://www.joomcoder.com/
+ * @copyright Copyright (C) 2007-2014 joomcoder (https://www.joomcoder.com). All rights reserved.
+ * @license   GNU/GPL http://www.gnu.org/copyleft/gpl.html
+ */
+
+use Joomcck\Assets\Webassets\Webassets;
+use Joomla\CMS\Language\Text;
+
+defined('_JEXEC') or die();
+
+
+extract($displayData);
+
+
+$wa = Webassets::$wa;
+
+$wa->useScript('com_joomcck.tom-select');
+$wa->useStyle('com_joomcck.tom-select');
+
+$cantAdd = $options['can_add'] ? 'true' : 'false';
+
+
+$selected = json_encode($selected);
+$default = str_replace(['"id":','"text":'],['id:','text:'],json_encode($default));
+
+
+?>
+
+<div id="select-tags-<?php echo $id ?>-container">
+	<select
+            id="<?php echo $id ?>"
+            name="<?php echo $name ?>[]"
+            multiple
+            data-placeholder="<?php echo Text::_('CADDTAGS') ?>"
+    >
+	</select>
+</div>
+
+<script>
+
+    new TomSelect("#<?php echo $id ?>",{
+        plugins: ['remove_button'], // add remove options feature
+        maxItems: <?php echo $options['max_items'] ?>, // max options user can select
+        maxOptions: <?php echo $options['suggestion_limit'] ?>, // max options to display in dropdown
+        create: <?php echo $cantAdd ?>, // allow user to add new ones
+        options: <?php echo $default ?>, // default options
+        items: <?php echo $selected ?>, // selected options
+        valueField: 'id',
+        labelField: 'text',
+        searchField : 'text',
+        load: function(query, callback) {
+
+            var url = '<?php echo $options['suggestion_url'] ?>';
+            fetch(url)
+                .then(response => response.json())
+                .then(json => {
+                    callback(json.result);
+                }).catch(()=>{
+                callback();
+            });
+
+        },
+    });
+
+</script>
+
+
--- libraries/mint/forms/fields/metags.php
+++ libraries/mint/forms/fields/metags.php
@@ -5,8 +5,10 @@
  * a component for Joomla! 1.7 - 2.5 CMS (http://www.joomla.org)
  * Author Website: https://www.joomcoder.com/
  * @copyright Copyright (C) 2012 joomcoder (https://www.joomcoder.com). All rights reserved.
- * @license GNU/GPL http://www.gnu.org/copyleft/gpl.html
+ * @license   GNU/GPL http://www.gnu.org/copyleft/gpl.html
  */
+
+use Joomla\CMS\Layout\LayoutHelper;
 
 defined('JPATH_PLATFORM') || die;
 
@@ -15,76 +17,80 @@ jimport('joomla.form.formfield');
 
 class JFormFieldMetags extends JFormField
 {
-    public $type = 'Metags';
+	public $type = 'Metags';
 
-    public function getInput()
-    {
-        $new   = JFile::exists(JPATH_ROOT . "/media/com_joomcck/vendors/flow/flow.js");
-        $app   = JFactory::getApplication();
-        $model = JModelLegacy::getInstance('Form', 'JoomcckModel');
-        $type  = $model->getRecordType($app->input->getInt('type_id'));
+	public function getInput()
+	{
 
-        if ($new) {
-            if (!$this->value) {
-                $this->value = '[]';
-            }
+		$app   = JFactory::getApplication();
+		$model = JModelLegacy::getInstance('Form', 'JoomcckModel');
+		$type  = $model->getRecordType($app->input->getInt('type_id'));
 
-            $default = [];
-            $value   = json_decode($this->value, 1);
-            foreach ($value as $k => $v) {
-                $default[] = [
-                    "id"   => $k,
-                    "text" => $v
-                ];
-            }
+		$default = $this->getDefault();
+		$selected = $this->getSelected();
 
-            $this->params = new JRegistry();
+		$this->params = new JRegistry();
 
-            $options['only_suggestions'] = 0;
-            $options['can_add']          = 1;
-            $options['can_delete']       = 1;
-            $options['suggestion_limit'] = 10;
-            $options['limit']            = $type->params->get('general.item_tags_max', 25);
-            $options['suggestion_url']   = 'index.php?option=com_joomcck&task=ajax.tags_list&tmpl=component';
+		$options['coma_separate']    = 0;
+		$options['only_values']      = 0;
+		$options['min_length']       = 1;
+		$options['case_sensitive']   = 0;
+		$options['unique']           = 1;
+		$options['highlight']        = 1;
+		$options['can_add']          = 1;
+		$options['can_delete']       = 1;
+		$options['only_suggestions'] = 0;
+		$options['suggestion_limit'] = 10;
+		$options['max_width']        = $this->params->get('params.max_width', 500);
+		$options['min_width']        = $this->params->get('params.min_width', 400);
+		$options['max_items']        = $type->params->get('general.item_tags_max', 25);
+		$options['limit']            = $type->params->get('general.item_tags_max', 25);
+		$options['suggestion_url']   = JUri::root().'index.php?option=com_joomcck&task=ajax.tags_list&tmpl=component';
 
-            return JHtml::_('mrelements.pills', "jform[tags]", "tags", $default, [], $options);
-        }
+		$data = [
+			'options' => $options,
+			'selected' => $selected,
+			'default' => $default,
+			'name' => 'jform[tags]',
+			'id' => 'tags'
+		];
 
-        if (!$this->value) {
-            $value = [];
-        } else {
-            $value = json_decode($this->value, 1);
-            if (empty($value)) {
-                $value = $this->value;
-            }
-        }
 
-        if (!is_array($value) && !empty($value)) {
-            $value = explode(',', $this->value);
-        }
-        ArrayHelper::clean_r($value);
-        $default = [];
-        foreach ($value as $tag) {
-            $default[$tag] = $tag;
-        }
+		// we use TomSelect JS lib
+		return LayoutHelper::render('core.fields.tomSelectTagsAjax',$data,null,['client' => 'site','component' => 'com_joomcck']);
 
-        $this->params = new JRegistry();
 
-        $options['coma_separate']  = 0;
-        $options['only_values']    = 0;
-        $options['min_length']     = 1;
-        $options['max_result']     = 10;
-        $options['case_sensitive'] = 0;
-        $options['unique']         = 1;
-        $options['highlight']      = 1;
+	}
 
-        $options['max_width'] = $this->params->get('params.max_width', 500);
-        $options['min_width'] = $this->params->get('params.min_width', 400);
-        $options['max_items'] = $type->params->get('general.item_tags_max', 25);
+	public function getSelected(){
+		if(empty($this->value))
+			return [];
 
-        $options['ajax_url']  = 'index.php?option=com_joomcck&task=ajax.tags_list&tmpl=component';
-        $options['ajax_data'] = '';
+		$values = [];
 
-        return JHtml::_('mrelements.listautocomplete', "jform[tags]", "tags", $default, null, $options);
-    }
+		foreach (json_decode($this->value) as $k => $v){
+
+			$values[] = [$k];
+		}
+
+		return $values;
+	}
+
+
+	public function getDefault(){
+
+		if(empty($this->value))
+			return [];
+
+		$values = [];
+
+		foreach (json_decode($this->value) as $k => $v){
+
+			$values[] = ['id' => $k, 'text' => $v];
+		}
+
+		return $values;
+
+	}
+
 }
