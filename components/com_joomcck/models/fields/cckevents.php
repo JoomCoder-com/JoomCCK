567a46d3074af5cc1c76a85fd93fa57a389cd235
ANDI Amine <andi.amine@gmail.com>
2023-05-17 17:36:00 +0100

    Fixed - Events field tooltip was not working Improved - Events inputs now use BS5

--- components/com_joomcck/models/fields/cckevents.php
+++ components/com_joomcck/models/fields/cckevents.php
@@ -7,12 +7,16 @@
  * @license GNU/GPL http://www.gnu.org/copyleft/gpl.html
  */
 
+use Joomla\CMS\HTML\HTMLHelper;
+
 defined('_JEXEC') or die();
 
 jimport('joomla.html.html');
 jimport('joomla.form.formfield');
 jimport('joomla.filesystem.folder');
 jimport('joomla.filesystem.file');
+
+HTMLHelper::_('bootstrap.tooltip', '.hasTooltip');
 
 class JFormFieldCckevents extends JFormField
 {
@@ -41,16 +45,16 @@ class JFormFieldCckevents extends JFormField
 		foreach($events as $event => $karma)
 		{
 			$access = JHtml::_('access.level', $this->name . "[{$event}][notif]", isset($this->value->$event->notif) ? $this->value->$event->notif : 2,
-				'class="col-md-12"', array(JHtml::_('select.option', '0', JText::_('XML_OPT_NOONE'))));
+				'class="form-select form-select-sm"', array(JHtml::_('select.option', '0', JText::_('XML_OPT_NOONE'))));
 			$act = JHtml::_('access.level', $this->name . "[{$event}][activ]", isset($this->value->$event->activ) ? $this->value->$event->activ : 2,
-				'class="col-md-12"', array(JHtml::_('select.option', '0', JText::_('XML_OPT_NOONE'))));
-			$karma1 = sprintf('<input class="input-mini" type="text" size="3" value="%d" name="%s[%s][karma1]">',
+				'class="form-select form-select-sm"', array(JHtml::_('select.option', '0', JText::_('XML_OPT_NOONE'))));
+			$karma1 = sprintf('<input class="form-control form-control-sm " type="text" size="3" value="%d" name="%s[%s][karma1]">',
 				isset($this->value->$event->karma1) ? $this->value->$event->karma1 : 0, $this->name, $event);
-			$karma2 = sprintf('<input class="input-mini" type="text" size="3" value="%d" name="%s[%s][karma2]">',
+			$karma2 = sprintf('<input class="form-control form-control-sm " type="text" size="3" value="%d" name="%s[%s][karma2]">',
 				isset($this->value->$event->karma2) ? $this->value->$event->karma2 : 0, $this->name, $event);
-			$msg = sprintf('<input class="input-medium hasTooltip" title="'.JText::_('EVENT_' . strtoupper($event)).'" type="text" value="%s" name="%s[%s][msg]">',
+			$msg = sprintf('<input class="form-control form-control-sm hasTooltip" title="'.JText::_('EVENT_' . strtoupper($event)).'" type="text" value="%s" name="%s[%s][msg]">',
 				isset($this->value->$event->msg) ? $this->value->$event->msg : 'EVENT_' . strtoupper($event), $this->name, $event);
-			$msg_pers = sprintf('<input class="input-medium hasTooltip" title="'.str_replace('%', '', JText::_('EVENT_' . strtoupper($event) . '_PERS')).'" type="text" value="%s" name="%s[%s][msg_pers]">',
+			$msg_pers = sprintf('<input class="form-control form-control-sm hasTooltip" title="'.str_replace('%', '', JText::_('EVENT_' . strtoupper($event) . '_PERS')).'" type="text" value="%s" name="%s[%s][msg_pers]">',
 				isset($this->value->$event->msg_pers) ? $this->value->$event->msg_pers : 'EVENT_' . strtoupper($event) . '_PERS', $this->name, $event);
 
 			$html[] = sprintf($patern, JText::_('EVENT_TYPE_'.$event), $access, $act, $karma[0] ? $karma1 : null, $karma[1] ? $karma2 : null, $msg, $msg_pers);
