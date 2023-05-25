e65131c1d46de797bcf42bb9e39ab89b8a4747e8
ANDI Amine <andi.amine@gmail.com>
2023-05-25 07:34:43 +0100

    Improved - checkboxes filter UI/UX improved to use BS5 [Checkbox field]

--- components/com_joomcck/fields/checkbox/tmpl/filter/autocomplete.php
+++ components/com_joomcck/fields/checkbox/tmpl/filter/autocomplete.php
@@ -14,7 +14,7 @@ $default = $this->default;
 ?>
 
 <?php if($this->params->get('params.total_limit') != 1):?>
-<select name="filters[<?php echo $this->key;?>][by]" data-bs-title="<?php echo JText::_('CSELECTFILTERCONDITION')?>" rel="tooltip">
+<select class="form-control" name="filters[<?php echo $this->key;?>][by]" title="<?php echo JText::_('CSELECTFILTERCONDITION')?>" rel="tooltip">
 	<option value="any" <?php if(isset($this->value['by']) && $this->value['by'] == 'any') echo 'selected="selected"';?>><?php echo JText::_('CRECORDHASANYVALUE')?></option>
 	<option value="all" <?php if(isset($this->value['by']) && $this->value['by'] == 'all') echo 'selected="selected"';?>><?php echo JText::_('CRECORDHASALLVALUES')?></option>
 </select>
