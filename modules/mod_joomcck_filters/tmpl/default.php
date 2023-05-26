3817789739cf092e9cd976043992c07de90fd558
ANDI Amine <andi.amine@gmail.com>
2023-05-27 00:13:29 +0100

    Improved - UI/UX to use BS5 [Layout Default] [Module Filters] Fixed - tooltip of field descriptio was not working [Module Filters]

--- modules/mod_joomcck_filters/tmpl/default.php
+++ modules/mod_joomcck_filters/tmpl/default.php
@@ -48,82 +48,90 @@ JHTML::_('bootstrap.tooltip', '*[rel^="tooltip"]');
 	<?php endif; ?>
 
 	<?php if(count($f_types) > 1): ?>
-		<legend>
-			<?php if($params->get('show_icons', 1)): ?>
-				<span class="float-start filter-icon"><?php echo HTMLFormatHelper::icon('block.png'); ?></span>
-			<?php endif; ?>
-			<?php echo $params->get('type_label'); ?>
-		</legend>
-		<div class="well well-small<?php echo($state->get('records.type') ? ' active' : NULL) ?>">
-			<?php if($params->get('filter_type_type') == 1): ?>
-				<?php foreach($f_types AS $type): ?>
-					<label class="checkbox">
-						<input id="type-<?php echo $type->id ?>" type="checkbox" name="filters[type][]" value="<?php echo $type->id ?>"<?php echo $type->filter_checked ?>>
-						<?php echo $type->name; ?>
-					</label>
-				<?php endforeach; ?>
-			<?php else : ?>
-				<?php echo JHtml::_('select.genericlist', $f_types, 'filters[type]', NULL, 'id', 'name', $state->get('records.type')); ?>
-			<?php endif; ?>
-		</div>
+		<div class="mb-3 typesFilter">
+            <label class="form-label">
+				<?php if($params->get('show_icons', 1)): ?>
+                    <span class="float-start filter-icon"><?php echo HTMLFormatHelper::icon('block.png'); ?></span>
+				<?php endif; ?>
+				<?php echo $params->get('type_label'); ?>
+            </label>
+            <div class="<?php echo($state->get('records.type') ? ' active' : NULL) ?>">
+				<?php if($params->get('filter_type_type') == 1): ?>
+					<?php foreach($f_types AS $type): ?>
+                        <div class="form-check">
+                            <input id="type-<?php echo $type->id ?>" type="checkbox" class="form-check-input" name="filters[type][]" value="<?php echo $type->id ?>"<?php echo $type->filter_checked ?>>
+                            <label for="type-<?php echo $type->id ?>" class="form-check-label"><?php echo $type->name; ?></label>
+                        </div>
+					<?php endforeach; ?>
+				<?php else : ?>
+					<?php echo JHtml::_('select.genericlist', $f_types, 'filters[type]', "class='form-select' ", 'id', 'name', $state->get('records.type')); ?>
+				<?php endif; ?>
+            </div>
+        </div>
 	<?php endif; ?>
 
 	<?php if($params->get('filter_tags_type')): ?>
-		<legend>
-			<?php if($params->get('show_icons', 1)): ?>
-				<span class="float-start filter-icon"><?php echo HTMLFormatHelper::icon('price-tag.png'); ?></span>
-			<?php endif; ?>
-			<?php echo $params->get('tag_label'); ?>
-		</legend>
-		<div class="well well-small<?php echo($state->get('records.tag') ? ' active' : NULL) ?>">
-			<?php if($params->get('filter_tags_type') == 1): ?>
-				<?php echo JHtml::_('tags.tagform', $section, $state->get('records.tag'), array('id' => 'module_tags')); ?>
-			<?php elseif($params->get('filter_tags_type') == 2): ?>
-				<?php echo JHtml::_('tags.tagcheckboxes', $section, $state->get('records.tag')); ?>
-			<?php elseif($params->get('filter_tags_type') == 3): ?>
-				<?php echo JHtml::_('tags.tagselect', $section, $state->get('records.tag')); ?>
-			<?php elseif($params->get('filter_tags_type') == 4): ?>
-				<?php echo JHtml::_('tags.tagpills', $section, $state->get('records.tag')); ?>
-			<?php endif; ?>
-		</div>
+		<div class="mb-3">
+            <label class="form-label">
+				<?php if($params->get('show_icons', 1)): ?>
+                    <span class="float-start filter-icon"><?php echo HTMLFormatHelper::icon('price-tag.png'); ?></span>
+				<?php endif; ?>
+				<?php echo $params->get('tag_label'); ?>
+            </label>
+            <div class="<?php echo($state->get('records.tag') ? ' active' : NULL) ?>">
+				<?php if($params->get('filter_tags_type') == 1): ?>
+					<?php echo JHtml::_('tags.tagform', $section, $state->get('records.tag'), array('id' => 'module_tags')); ?>
+				<?php elseif($params->get('filter_tags_type') == 2): ?>
+					<?php echo JHtml::_('tags.tagcheckboxes', $section, $state->get('records.tag')); ?>
+				<?php elseif($params->get('filter_tags_type') == 3): ?>
+					<?php echo JHtml::_('tags.tagselect', $section, $state->get('records.tag')); ?>
+				<?php elseif($params->get('filter_tags_type') == 4): ?>
+					<?php echo JHtml::_('tags.tagpills', $section, $state->get('records.tag')); ?>
+				<?php endif; ?>
+            </div>
+        </div>
 	<?php endif; ?>
 
 	<?php if($params->get('filter_users_type')): ?>
-		<legend>
-			<?php if($params->get('show_icons', 1)): ?>
-				<span class="float-start filter-icon"><?php echo HTMLFormatHelper::icon('user.png'); ?></span>
-			<?php endif; ?>
-			<?php echo $params->get('user_label'); ?>
-		</legend>
-		<div class="well well-small<?php echo($state->get('records.user') ? ' active' : NULL) ?>">
-			<?php if($params->get('filter_users_type') == 1): ?>
-				<?php echo JHtml::_('cusers.form', $section, $state->get('records.user'), array('id' => 'module_users')); ?>
-			<?php elseif($params->get('filter_users_type') == 2): ?>
-				<?php echo JHtml::_('cusers.checkboxes', $section, $state->get('records.user')); ?>
-			<?php elseif($params->get('filter_users_type') == 3): ?>
-				<?php echo JHtml::_('cusers.select', $section, $state->get('records.user')); ?>
-			<?php endif; ?>
-		</div>
+		<div class="mb-3">
+            <label class="form-label">
+				<?php if($params->get('show_icons', 1)): ?>
+                    <span class="float-start filter-icon"><?php echo HTMLFormatHelper::icon('user.png'); ?></span>
+				<?php endif; ?>
+				<?php echo $params->get('user_label'); ?>
+            </label>
+            <div class="<?php echo($state->get('records.user') ? ' active' : NULL) ?>">
+				<?php if($params->get('filter_users_type') == 1): ?>
+					<?php echo JHtml::_('cusers.form', $section, $state->get('records.user'), array('id' => 'module_users')); ?>
+				<?php elseif($params->get('filter_users_type') == 2): ?>
+					<?php echo JHtml::_('cusers.checkboxes', $section, $state->get('records.user')); ?>
+				<?php elseif($params->get('filter_users_type') == 3): ?>
+					<?php echo JHtml::_('cusers.select', $section, $state->get('records.user')); ?>
+				<?php endif; ?>
+            </div>
+        </div>
 	<?php endif; ?>
 
 	<?php if($params->get('filter_category_type')): ?>
-		<legend>
-			<?php if($params->get('show_icons', 1)): ?>
-				<span class="float-start filter-icon"><?php echo HTMLFormatHelper::icon('category.png'); ?></span>
-			<?php endif; ?>
-			<?php echo $params->get('category_label'); ?>
-		</legend>
-		<div class="well well-small<?php echo($state->get('records.category') ? ' active' : NULL) ?>">
-			<?php if($params->get('filter_category_type') == 1): ?>
-				<?php echo JHtml::_('categories.form', $section, $state->get('records.category'), array('empty_cats' => $params->get('filter_empty_cats', 1))); ?>
-			<?php elseif($params->get('filter_category_type') == 2): ?>
-				<?php echo JHtml::_('categories.checkboxes', $section, $state->get('records.category'), array('columns' => 3, 'empty_cats' => $params->get('filter_empty_cats', 1))); ?>
-			<?php elseif($params->get('filter_category_type') == 3): ?>
-				<?php echo JHtml::_('categories.select', $section, $state->get('records.category'), array('multiple' => 0, 'empty_cats' => $params->get('filter_empty_cats', 1))); ?>
-			<?php elseif($params->get('filter_category_type') == 4): ?>
-				<?php echo JHtml::_('categories.select', $section, $state->get('records.category'), array('multiple' => 1, 'size' => 25, 'empty_cats' => $params->get('filter_empty_cats', 1))); ?>
-			<?php endif; ?>
-		</div>
+		<div class="mb-3">
+            <label class="form-label">
+				<?php if($params->get('show_icons', 1)): ?>
+                    <span class="float-start filter-icon"><?php echo HTMLFormatHelper::icon('category.png'); ?></span>
+				<?php endif; ?>
+				<?php echo $params->get('category_label'); ?>
+            </label>
+            <div class="<?php echo($state->get('records.category') ? ' active' : NULL) ?>">
+				<?php if($params->get('filter_category_type') == 1): ?>
+					<?php echo JHtml::_('categories.form', $section, $state->get('records.category'), array('empty_cats' => $params->get('filter_empty_cats', 1))); ?>
+				<?php elseif($params->get('filter_category_type') == 2): ?>
+					<?php echo JHtml::_('categories.checkboxes', $section, $state->get('records.category'), array('columns' => 3, 'empty_cats' => $params->get('filter_empty_cats', 1))); ?>
+				<?php elseif($params->get('filter_category_type') == 3): ?>
+					<?php echo JHtml::_('categories.select', $section, $state->get('records.category'), array('multiple' => 0, 'empty_cats' => $params->get('filter_empty_cats', 1))); ?>
+				<?php elseif($params->get('filter_category_type') == 4): ?>
+					<?php echo JHtml::_('categories.select', $section, $state->get('records.category'), array('multiple' => 1, 'size' => 25, 'empty_cats' => $params->get('filter_empty_cats', 1))); ?>
+				<?php endif; ?>
+            </div>
+        </div>
 	<?php endif; ?>
 
 	<?php foreach($filters AS $filter): ?>
@@ -131,21 +139,23 @@ JHTML::_('bootstrap.tooltip', '*[rel^="tooltip"]');
 			continue; ?>
 		<?php $f = $filter->onRenderFilter($section, ($params->get('filter_fields_template') == 'section' ? FALSE : TRUE)) ?>
 		<?php if(trim($f)): ?>
-			<legend>
-				<?php if($params->get('show_icons', 1) && $filter->params->get('core.icon')): ?>
-					<span class="float-start filter-icon"><?php echo HTMLFormatHelper::icon($filter->params->get('core.icon')); ?></span>
-				<?php endif; ?>
-				<?php echo $filter->label; ?>
-				<?php if($filter->params->get('params.filter_descr')): ?>
-					<small rel="tooltip" data-bs-title="<?php echo JText::_($filter->params->get('params.filter_descr')); ?>"><i class="icon-help"></i></small>
-				<?php endif; ?>
-			</legend>
-			<div class="well well-small<?php echo ($filter->isFilterActive() ? ' active' : NULL) ?>">
-				<?php if($filter->isFilterActive()): ?>
-					<!-- <img class="filter-close" onclick="Joomcck.cleanFilter('filter_<?php echo $filter->key ?>')" rel="tooltip" data-bs-title="<?php echo JText::_('CDELETEFILTER') ?>" src="<?php echo JUri::root(TRUE) ?>/media/com_joomcck/icons/16/cross-circle.png">-->
-				<?php endif; ?>
-				<?php echo $f; ?>
-			</div>
+			<div class="mb-3">
+                <label class="form-label">
+					<?php if($params->get('show_icons', 1) && $filter->params->get('core.icon')): ?>
+                        <span class="float-start filter-icon"><?php echo HTMLFormatHelper::icon($filter->params->get('core.icon')); ?></span>
+					<?php endif; ?>
+					<?php echo $filter->label; ?>
+					<?php if($filter->params->get('params.filter_descr')): ?>
+                        <small rel="tooltip" title="<?php echo JText::_($filter->params->get('params.filter_descr')); ?>"><i class="fas fa-info-circle text-muted"></i></small>
+					<?php endif; ?>
+                </label>
+                <div class="<?php echo ($filter->isFilterActive() ? ' active' : NULL) ?>">
+					<?php if($filter->isFilterActive()): ?>
+                        <!-- <img class="filter-close" onclick="Joomcck.cleanFilter('filter_<?php echo $filter->key ?>')" rel="tooltip" data-bs-title="<?php echo JText::_('CDELETEFILTER') ?>" src="<?php echo JUri::root(TRUE) ?>/media/com_joomcck/icons/16/cross-circle.png">-->
+					<?php endif; ?>
+					<?php echo $f; ?>
+                </div>
+            </div>
 		<?php endif; ?>
 
 	<?php endforeach; ?>
@@ -162,8 +172,8 @@ JHTML::_('bootstrap.tooltip', '*[rel^="tooltip"]');
 	<input type="hidden" name="limitstart" value="0">
 
 	<div class="form-actions">
-		<button type="submit" class="btn btn-primary btn-large">
-			<?php echo JText::_('CSEARCH'); ?>
+		<button type="submit" class="btn btn-outline-success">
+			<i class="fas fa-search"></i> <?php echo JText::_('CSEARCH'); ?>
 		</button>
 	</div>
 </form>
