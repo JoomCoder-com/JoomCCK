212da4f63c75972cd4dc5ad5d931e805f0c2c1b6
ANDI Amine <andi.amine@gmail.com>
2023-05-26 10:53:59 +0100

    Fixed - user section page settings was not saving Improved - user section page settings UI/UX

--- components/com_joomcck/views/options/tmpl/section.php
+++ components/com_joomcck/views/options/tmpl/section.php
@@ -22,9 +22,18 @@ $params = $this->section->params;
 
 
 <div class="page-header">
+	<?php if($back):?>
+        <div class="float-end">
+            <button type="button" class="btn btn-light border" onclick="location.href = '<?php echo $back;?>'">
+				<?php echo HTMLFormatHelper::icon('arrow-180.png');  ?>
+				<?php echo JText::_('CBACKTOSECTION'); ?>
+            </button>
+        </div>
+	<?php endif;?>
 <h1 class="title">
 	<?php echo JText::sprintf('CUSERSECTIONSETTINGS', $this->section->name);?>
 </h1>
+
 </div>
 
 <?php if(!$params->get('personalize.allow_section_set', 1)):?>
@@ -34,117 +43,89 @@ $params = $this->section->params;
 <?php else :?>
 
 <form name="adminForm" id="adminForm" method="post" class="form-horizontal">
-	
-	<?php if($back):?>
-	<div class="btn-toolbar clearfix">
-		<div class="float-end">
-		<button type="button" class="btn" onclick="location.href = '<?php echo $back;?>'">
-			<?php echo HTMLFormatHelper::icon('arrow-180.png');  ?>
-			<?php echo JText::_('CBACKTOSECTION'); ?>
-		</button>
-		</div>
-	</div>
-	<div class="clearfix"> </div>
-	<?php endif;?>
-	
+
 	<div class="row">
 		<?php if($params->get('personalize.allow_change_header', 1)):?>
-		<div class="control-group">
-			<div class="control-label col-md-2"><?php echo JText::_('CMYHOMEPAGETITLE'); ?></div>
-			<div class="controls">
-				<input type="text" name="jform[title]" size="40" value="<?php echo $this->options->get('title');?>">
-			</div>
+		<div class="mb-3">
+			<label for="userTitle" class="form-label"><?php echo JText::_('CMYHOMEPAGETITLE'); ?></label>
+            <input id="userTitle" type="text" class="form-control" name="jform[title]" size="40" value="<?php echo $this->options->get('title');?>">
 		</div>
 		<?php endif; ?>
 		<?php if($params->get('personalize.allow_change_descr', 1)):?>
-		<div class="control-group">
-			<div class="control-label col-md-2"><span rel="tooltip" data-bs-title="<?php echo JText::_('CMYHOMEPAGEMSGDESCR');?>"><?php echo JText::_('CMYHOMEPAGEMSG'); ?></span></div>
-			<div class="controls">
-				<textarea id="jform_description" name="jform[description]" cols="30" rows="4"><?php echo $this->options->get('description');?></textarea>
-			</div>
+		<div class="mb-3">
+            <label for="jform_description" rel="tooltip" class="form-label" title="<?php echo JText::_('CMYHOMEPAGEMSGDESCR');?>"><?php echo JText::_('CMYHOMEPAGEMSG'); ?></label>
+            <textarea class="form-control" id="jform_description" name="jform[description]" cols="30" rows="4"><?php echo $this->options->get('description');?></textarea>
 		</div>
 		<?php endif; ?>
 
 		<?php if($params->get('personalize.allow_access_control_add', 1) && $this->section->params->get('personalize.post_anywhere')): ?>
-			<div class="control-group">
-				<div class="control-label col-md-2"><?php echo JText::_('CWHOCANPOSTINMYHAMOPAGE'); ?></div>
-				<div class="controls">
-					<select name="jform[who_post]" >
-						<option value="0" <?php if($this->options->get('who_post') == '0') echo 'selected'; ?> ><?php echo JText::_('CONLYAUTHOR');?></option>
-						<option value="2" <?php if($this->options->get('who_post') == '2') echo 'selected'; ?> ><?php echo JText::_('CEVERYONE');?></option>
-						<?php if($this->section->params->get('events.subscribe_user')): ?>
-						<option value="1" <?php if($this->options->get('who_post') == '1') echo 'selected'; ?> ><?php echo JText::_('CONLYSUBSCRIBED');?></option>
-						<?php endif;?>
-					</select>
-				</div>
+			<div class="mb-3">
+				<label for="userWhoPost" class="form-label"><?php echo JText::_('CWHOCANPOSTINMYHAMOPAGE'); ?></label>
+                <select id="userWhoPost" class="form-select" name="jform[who_post]" >
+                    <option value="0" <?php if($this->options->get('who_post') == '0') echo 'selected'; ?> ><?php echo JText::_('CONLYAUTHOR');?></option>
+                    <option value="2" <?php if($this->options->get('who_post') == '2') echo 'selected'; ?> ><?php echo JText::_('CEVERYONE');?></option>
+					<?php if($this->section->params->get('events.subscribe_user')): ?>
+                        <option value="1" <?php if($this->options->get('who_post') == '1') echo 'selected'; ?> ><?php echo JText::_('CONLYSUBSCRIBED');?></option>
+					<?php endif;?>
+                </select>
 			</div>
 		<?php endif;?>
 		
 		<?php if($params->get('personalize.allow_access_control', 1) && $this->section->params->get('events.subscribe_user')): ?>
-			<div class="control-group">
-				<div class="control-label col-md-2"><?php echo JText::_('CWHOCANVIEWMYBOOKMARKS'); ?></div>
-				<div class="controls">
-					<select name="jform[who_view_bookmarked]" >
-						<option value="all" <?php if($this->options->get('who_view_bookmarked') == 'all') echo 'selected'; ?> ><?php echo JText::_('CEVERYONE');?></option>
-						<option value="subscribed" <?php if($this->options->get('who_view_bookmarked') == 'subscribed') echo 'selected'; ?> ><?php echo JText::_('CONLYSUBSCRIBED');?></option>
-					</select>
-				</div>
+			<div class="mb-3">
+				<label for="userWhoViewBookmarked" class="form-label"><?php echo JText::_('CWHOCANVIEWMYBOOKMARKS'); ?></label>
+                <select id="userWhoViewBookmarked" class="form-select" name="jform[who_view_bookmarked]" >
+                    <option value="all" <?php if($this->options->get('who_view_bookmarked') == 'all') echo 'selected'; ?> ><?php echo JText::_('CEVERYONE');?></option>
+                    <option value="subscribed" <?php if($this->options->get('who_view_bookmarked') == 'subscribed') echo 'selected'; ?> ><?php echo JText::_('CONLYSUBSCRIBED');?></option>
+                </select>
 			</div>
-			<div class="control-group">
-				<div class="control-label col-md-2"><?php echo JText::_('CWHOCANVIEWMYRATEDRECORDS'); ?></div>
-				<div class="controls">
-					<select name="jform[who_view_rated]" >
-						<option value="all" <?php if($this->options->get('who_view_rated') == 'all') echo 'selected'; ?> ><?php echo JText::_('CEVERYONE');?></option>
-						<option value="subscribed" <?php if($this->options->get('who_view_rated') == 'subscribed') echo 'selected'; ?> ><?php echo JText::_('CONLYSUBSCRIBED');?></option>
-					</select>
-				</div>
+			<div class="mb-3">
+				<label class="form-label" for="userWhoViewRated"><?php echo JText::_('CWHOCANVIEWMYRATEDRECORDS'); ?></label>
+                <select id="userWhoViewRated" class="form-select" name="jform[who_view_rated]" >
+                    <option value="all" <?php if($this->options->get('who_view_rated') == 'all') echo 'selected'; ?> ><?php echo JText::_('CEVERYONE');?></option>
+                    <option value="subscribed" <?php if($this->options->get('who_view_rated') == 'subscribed') echo 'selected'; ?> ><?php echo JText::_('CONLYSUBSCRIBED');?></option>
+                </select>
 			</div>
-			<div class="control-group">
-				<div class="control-label col-md-2"><?php echo JText::_('CWHOCANVIEWMYCOMMENTEDRECORDS'); ?></div>
-				<div class="controls">
-					<select name="jform[who_view_commented]" >
-						<option value="all" <?php if($this->options->get('who_view_commented') == 'all') echo 'selected'; ?> ><?php echo JText::_('CEVERYONE');?></option>
-						<option value="subscribed" <?php if($this->options->get('who_view_commented') == 'subscribed') echo 'selected'; ?> ><?php echo JText::_('CONLYSUBSCRIBED');?></option>
-					</select>
-				</div>
+			<div class="mb-3">
+				<label class="form-label" for="userWhoViewCommented"><?php echo JText::_('CWHOCANVIEWMYCOMMENTEDRECORDS'); ?></label>
+                <select id="userWhoViewCommented" class="form-select" name="jform[who_view_commented]" >
+                    <option value="all" <?php if($this->options->get('who_view_commented') == 'all') echo 'selected'; ?> ><?php echo JText::_('CEVERYONE');?></option>
+                    <option value="subscribed" <?php if($this->options->get('who_view_commented') == 'subscribed') echo 'selected'; ?> ><?php echo JText::_('CONLYSUBSCRIBED');?></option>
+                </select>
 			</div>
-			<div class="control-group">
-				<div class="control-label col-md-2"><?php echo JText::_('CWHOCANVIEWMYVISITEDRECORDS'); ?></div>
-				<div class="controls">
-					<select name="jform[who_view_visited]" >
-						<option value="all" <?php if($this->options->get('who_view_visited') == 'all') echo 'selected'; ?> ><?php echo JText::_('CEVERYONE');?></option>
-						<option value="subscribed" <?php if($this->options->get('who_view_visited') == 'subscribed') echo 'selected'; ?> ><?php echo JText::_('CONLYSUBSCRIBED');?></option>
-					</select>
-				</div>
+			<div class="mb-3">
+				<label for="userWhoViewVisited" class="form-label"><?php echo JText::_('CWHOCANVIEWMYVISITEDRECORDS'); ?></label>
+                <select class="form-select" id="userWhoViewVisited" class="form-select" name="jform[who_view_visited]" >
+                    <option value="all" <?php if($this->options->get('who_view_visited') == 'all') echo 'selected'; ?> ><?php echo JText::_('CEVERYONE');?></option>
+                    <option value="subscribed" <?php if($this->options->get('who_view_visited') == 'subscribed') echo 'selected'; ?> ><?php echo JText::_('CONLYSUBSCRIBED');?></option>
+                </select>
 			</div>
-			<div class="control-group">
-				<div class="control-label col-md-2"><?php echo JText::_('CWHOCANVIEWMYFOLLOWEDRECORDS'); ?></div>
-				<div class="controls">
-					<select name="jform[who_view_followed]" >
-						<option value="all" <?php if($this->options->get('who_view_followed') == 'all') echo 'selected'; ?> ><?php echo JText::_('CEVERYONE');?></option>
-						<option value="subscribed" <?php if($this->options->get('who_view_followed') == 'subscribed') echo 'selected'; ?> ><?php echo JText::_('CONLYSUBSCRIBED');?></option>
-					</select>
-				</div>
+			<div class="mb-3">
+				<label for="userWhoViewFollowed" class="form-label"><?php echo JText::_('CWHOCANVIEWMYFOLLOWEDRECORDS'); ?></label>
+                <select class="form-select" id="userWhoViewFollowed"  class="form-select" name="jform[who_view_followed]" >
+                    <option value="all" <?php if($this->options->get('who_view_followed') == 'all') echo 'selected'; ?> ><?php echo JText::_('CEVERYONE');?></option>
+                    <option value="subscribed" <?php if($this->options->get('who_view_followed') == 'subscribed') echo 'selected'; ?> ><?php echo JText::_('CONLYSUBSCRIBED');?></option>
+                </select>
 			</div>
 		<?php endif;?>
 	</div>
 			
 
-	<div class="form-actions">
-		<button class="btn" onclick="Joomla.submitbutton('options.savesectionoptions')">
+	<div class="form-actions mb-3">
+		<button class="btn btn-outline-success" onclick="Joomla.submitbutton('options.savesectionoptions')">
 			<?php echo HTMLFormatHelper::icon('disk.png');  ?>
 			<?php echo JText::_('CSAVE');?>
 		</button>
 
 		<?php if($back):?>
-			<button class="btn" type="button" onclick="Joomla.submitbutton('options.savesectionoptionsclose')">
+			<button class="btn btn-light border" type="button" onclick="Joomla.submitbutton('options.savesectionoptionsclose')">
 				<?php echo HTMLFormatHelper::icon('disk--minus.png');  ?>
 				<?php echo JText::_('CSAVECLOSE');?>
 			</button>
 		<?php endif;?>
 	</div>
 
-	<input type="hidden" name="task" value="" />
+	<input type="hidden" name="task" value="options.savesectionoptions" />
 	<?php echo JHtml::_('form.token'); ?>
 </form>
 <script type="text/javascript">
