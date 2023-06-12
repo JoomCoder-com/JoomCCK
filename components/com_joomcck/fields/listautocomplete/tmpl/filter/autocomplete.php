ad333128419e5204b0e91813fbf23da490fbc671
ANDI Amine <andi.amine@gmail.com>
2023-06-13 00:51:17 +0100

    Improved - autocomplete UI/UX improved [List autocomplete field]

--- components/com_joomcck/fields/listautocomplete/tmpl/filter/autocomplete.php
+++ components/com_joomcck/fields/listautocomplete/tmpl/filter/autocomplete.php
@@ -11,18 +11,6 @@ defined('_JEXEC') or die();
 JHTML::_('bootstrap.tooltip', '*[rel^="tooltip"]');
 
 $default = $this->default;
-?>
-
-<?php if($this->params->get('params.total_limit') != 1):?>
-<select name="filters[<?php echo $this->key;?>][by]" data-bs-title="<?php echo JText::_('CSELECTFILTERCONDITION')?>" rel="tooltip">
-	<option value="any" <?php if(isset($this->value['by']) && $this->value['by'] == 'any') echo 'selected="selected"';?>><?php echo JText::_('CRECORDHASANYVALUE')?></option>
-	<option value="all" <?php if(isset($this->value['by']) && $this->value['by'] == 'all') echo 'selected="selected"';?>><?php echo JText::_('CRECORDHASALLVALUES')?></option>
-</select>
-<Br>
-<?php endif;?>
-
-<?php
-$default = $this->default;
 
 $options['only_suggestions'] = 1;
 $options['can_add'] = 1;
@@ -30,4 +18,26 @@ $options['can_delete'] = 1;
 $options['suggestion_limit'] = $this->params->get('params.max_result', 10);
 $options['suggestion_url'] = "index.php?option=com_joomcck&task=ajax.field_call&tmpl=component&field_id={$this->id}&func=onFilterGetValues&section_id={$section->id}&field={$this->type}";
 
-echo JHtml::_('mrelements.pills', "filters[{$this->key}][value]", "filter_" . $this->id, $default, array(), $options);
+?>
+
+
+<div class="row">
+    <div class="col-md-6">
+		<?php
+
+		echo JHtml::_('mrelements.pills', "filters[{$this->key}][value]", "filter_" . $this->id, $default, array(), $options);
+
+		?>
+    </div>
+	<?php if ($this->params->get('params.total_limit') != 1): ?>
+
+        <div class="col-md-6 mt-sm-3 mt-md-0">
+            <select class="form-select" name="filters[<?php echo $this->key; ?>][by]"
+                    title="<?php echo JText::_('CFILTERCONDITION') ?>" rel="tooltip">
+                <option value="any" <?php if (isset($this->value['by']) && $this->value['by'] == 'any') echo 'selected="selected"'; ?>><?php echo JText::_('CRECORDHASANYVALUE') ?></option>
+                <option value="all" <?php if (isset($this->value['by']) && $this->value['by'] == 'all') echo 'selected="selected"'; ?>><?php echo JText::_('CRECORDHASALLVALUES') ?></option>
+            </select>
+        </div>
+
+	<?php endif; ?>
+</div>
