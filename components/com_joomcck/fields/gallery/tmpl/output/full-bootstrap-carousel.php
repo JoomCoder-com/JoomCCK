e086232e98befd61ce57aae1de4ea7e9f78a8fea
ANDI Amine <andi.amine@gmail.com>
2023-06-27 09:11:13 +0100

    Improved - boostrap carousel now use BS5 [Gallery Field]

--- components/com_joomcck/fields/gallery/tmpl/output/full-bootstrap-carousel.php
+++ components/com_joomcck/fields/gallery/tmpl/output/full-bootstrap-carousel.php
@@ -17,51 +17,60 @@ $key = $this->id . '-' . $record->id;
 $dir = JComponentHelper::getParams('com_joomcck')->get('general_upload') . DIRECTORY_SEPARATOR . $this->params->get('params.subfolder', $this->field_type) . DIRECTORY_SEPARATOR;
 ?>
 
+    <div id="carusel-<?php echo $key ?>" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
 
-	<div id="carusel-<?php echo $key ?>" class="carousel">
-		<ol class="carousel-indicators">
-			<?php foreach($this->value as $picture_index => $file) : ?>
-				<li data-target="#carusel-<?php echo $key ?>" data-slide-to="<?php echo $picture_index ?>"></li>
-			<?php endforeach; ?>
-		</ol>
-		<!-- Carousel items -->
-		<div class="carousel-inner">
-			<?php
-			foreach($this->value as $picture_index => $file)
-			{
-				$picture = $dir . $file['fullpath'];
-				$url     = CImgHelper::getThumb($picture, $this->params->get('params.full_width', 100), $this->params->get('params.full_height', 100), 'gallery' . $key, $record->user_id,
-					array(
-						 'mode'       => $this->params->get('params.full_mode', 6),
-						 'strache'    => $this->params->get('params.full_stretch', 1),
-						 'background' => $this->params->get('params.thumbs_background_color', "#000000"),
-						 'quality'    => $this->params->get('params.full_quality', 80)
-					));
-				echo '<div class="item"><img src="' . $url . '"></div>';
-			}
-			?>
-		</div>
-		<!-- Carousel nav -->
-		<a class="carousel-control left" href="#carusel-<?php echo $key ?>" data-slide="prev">&lsaquo;</a>
-		<a class="carousel-control right" href="#carusel-<?php echo $key ?>" data-slide="next">&rsaquo;</a>
-	</div>
-	<script type="text/javascript">
-		if(typeof jQuery != 'undefined' && typeof MooTools != 'undefined') {
-			Element.implement({
-				slide: function(how, mode) {
-					return this;
-				}
-			});
-		}
-		jQuery('#carusel-<?php echo $key ?>').carousel();
-		jQuery('.item:first').addClass('active');
-		jQuery('li[data-slide-to="0"]').addClass('active');
-	</script>
+	        <?php foreach($this->value as $picture_index => $file) : ?>
+
+                <button type="button" data-bs-target="#carusel-<?php echo $key ?>" data-bs-slide-to="<?php echo $picture_index ?>" class="<?php echo $picture_index == 0 ? 'active' : '' ?>"</button>
+
+	        <?php endforeach; ?>
+
+        </div>
+        <div class="carousel-inner">
+
+
+	        <?php
+	        foreach($this->value as $picture_index => $file)
+	        {
+		        $picture = $dir . $file['fullpath'];
+		        $url     = CImgHelper::getThumb($picture, $this->params->get('params.full_width', 100), $this->params->get('params.full_height', 100), 'gallery' . $key, $record->user_id,
+			        array(
+				        'mode'       => $this->params->get('params.full_mode', 6),
+				        'strache'    => $this->params->get('params.full_stretch', 1),
+				        'background' => $this->params->get('params.thumbs_background_color', "#000000"),
+				        'quality'    => $this->params->get('params.full_quality', 80)
+			        ));
+
+                ?>
+
+		        <div class="carousel-item <?php echo $picture_index == 0 ? 'active' : '' ?>">
+                    <img src="<?php echo $url ?>">
+                </div>
+
+
+	        <?php }
+	        ?>
+
+
+        </div>
+
+        <button class="carousel-control-prev" type="button" data-bs-target="#carusel-<?php echo $key ?>" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carusel-<?php echo $key ?>" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
 
 
 <?php if($this->params->get('params.download_all', 0) == 1): ?>
 	<div class="clearfix"></div>
-	<a class="btn btn-success" href="<?php echo Url::task('files.download&fid=' . $this->id . '&rid=' . $record->id, 0); ?>">
-		<?php echo JText::_('CDOWNLOADALL') ?>
-	</a>
+	<div class="my-2">
+        <a class="btn btn-outline-success btn-sm" href="<?php echo Url::task('files.download&fid=' . $this->id . '&rid=' . $record->id, 0); ?>">
+			<?php echo JText::_('CDOWNLOADALL') ?>
+        </a>
+    </div>
 <?php endif; ?>
