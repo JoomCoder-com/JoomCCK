9fac8896b0fdb3304aa5e09598b89475c23519b5
ANDI Amine <andi.amine@gmail.com>
2024-02-03 18:33:50 +0100

    New Feature - more rating templates added by @Giorgi625

---
+++ components/com_joomcck/views/rating_tmpls/rating_fancystar.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * JoomCCK Rating template by Joomla.ge
+ * a component for Joomla! 5 CMS (http://www.joomla.org)
+ * Author Website: http://joomla.ge/
+ * @copyright Copyright (C) 2012 MintJoomla (http://joomla.ge). All rights reserved.
+ * @license GNU/GPL http://www.gnu.org/copyleft/gpl.html
+ */
+
+defined('_JEXEC') or die();
+$key = $vars->index . '_' . $vars->rid;
+
+?>
+<style>
+	#<?php echo $vars->rating_ident; ?>_rateBox span {
+		background: url('<?php  echo $vars->img_path ?>fancystar-gray.png') no-repeat;
+		width: 22px;
+		height: 20px;
+		display: inline-block;
+	}
+
+	#<?php echo $vars->rating_ident; ?>_rateBox span.on {
+		background: url('<?php  echo $vars->img_path ?>fancystar.png') no-repeat;
+		display: inline-block;
+	}
+</style>
+
+<div id="<?php echo $vars->rating_ident; ?>_rateBox"></div>
+
+<script type="text/javascript">
+	var newRating<?php echo $key;?> = felixRating.newRating('<?php echo $vars->rating_ident; ?>_rateBox', <?php echo $vars->rating_active; ?>);
+	newRating<?php echo $key;?>.setStars({ 
+		"10": '<?php echo JText::_("1")?>',
+		"20": '<?php echo JText::_("2")?>',
+		"30": '<?php echo JText::_("3")?>',
+		"40": '<?php echo JText::_("4")?>',
+		"50": '<?php echo JText::_("5")?>',
+		"60": '<?php echo JText::_("6")?>',
+		"70": '<?php echo JText::_("7")?>',
+		"80": '<?php echo JText::_("8")?>',
+		"90": '<?php echo JText::_("9")?>',
+		"100": '<?php echo JText::_("10")?>'
+		});
+	newRating<?php echo $key;?>.setCurrentStar("<?php echo $vars->rating_current; ?>");
+	newRating<?php echo $key;?>.setIndex(<?php echo $vars->index?>);
+	<?php if( $vars->callbackfunction ){ ?>
+	newRating<?php echo $key;?>.setSedingFunction(<?php echo $vars->callbackfunction; ?>, '<?php echo $vars->prod_id; ?>');
+	<?php } ?>
+</script>
---
+++ components/com_joomcck/views/rating_tmpls/rating_star.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Cobalt by MintJoomla
+ * a component for Joomla! 1.7 - 2.5 CMS (http://www.joomla.org)
+ * Author Website: http://www.mintjoomla.com/
+ * @copyright Copyright (C) 2012 MintJoomla (http://www.mintjoomla.com). All rights reserved.
+ * @license GNU/GPL http://www.gnu.org/copyleft/gpl.html
+ */
+
+defined('_JEXEC') or die();
+$key = $vars->index . '_' . $vars->rid;
+
+?>
+<style>
+	#<?php echo $vars->rating_ident; ?>_rateBox span {
+		background: url('<?php  echo $vars->img_path ?>blue-star.png') no-repeat;
+		width: 22px;
+		height: 22px;
+		display: inline-block;
+	}
+
+	#<?php echo $vars->rating_ident; ?>_rateBox span.on {
+		background: url('<?php  echo $vars->img_path ?>red-star.png') no-repeat;
+		display: inline-block;
+	}
+</style>
+
+<div id="<?php echo $vars->rating_ident; ?>_rateBox"></div>
+
+<script type="text/javascript">
+	var newRating<?php echo $key;?> = felixRating.newRating('<?php echo $vars->rating_ident; ?>_rateBox', <?php echo $vars->rating_active; ?>);
+	newRating<?php echo $key;?>.setStars({ 
+		"20": '<?php echo JText::_("20%")?>',
+		"40": '<?php echo JText::_("40%")?>',
+		"60": '<?php echo JText::_("60%")?>',
+		"80": '<?php echo JText::_("80%")?>',
+		"100": '<?php echo JText::_("100%")?>'
+		});
+	newRating<?php echo $key;?>.setCurrentStar("<?php echo $vars->rating_current; ?>");
+	newRating<?php echo $key;?>.setIndex(<?php echo $vars->index?>);
+	<?php if( $vars->callbackfunction ){ ?>
+	newRating<?php echo $key;?>.setSedingFunction(<?php echo $vars->callbackfunction; ?>, '<?php echo $vars->prod_id; ?>');
+	<?php } ?>
+</script>
