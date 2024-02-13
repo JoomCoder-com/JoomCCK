d91585fe8b101639a343bc9d7355b46a1684fc65
ANDI Amine <andi.amine@gmail.com>
2024-02-13 01:21:58 +0100

    Improved - Events to log params UI/UX improved [Audit Trail Settings]
    (cherry picked from commit d0efcb068bf020a21ef0e07b425b52e9f9619bdb)

--- components/com_joomcck/models/fields/cckauditevents.php
+++ components/com_joomcck/models/fields/cckauditevents.php
@@ -21,15 +21,18 @@ class JFormFieldCckAuditEvents extends \Joomla\CMS\Form\FormField
 	{
 
 		$options   = array();
-		$options[] = \Joomla\CMS\HTML\HTMLHelper::_('select.option', '0', 'JNO', 'value', 'text');
-		$options[] = \Joomla\CMS\HTML\HTMLHelper::_('select.option', '1', 'JYES', 'value', 'text');
+		$options[] = \Joomla\CMS\HTML\HTMLHelper::_('select.option', '0', \Joomla\CMS\Language\Text::_('JNO'), 'value', 'text');
+		$options[] = \Joomla\CMS\HTML\HTMLHelper::_('select.option', '1', \Joomla\CMS\Language\Text::_('JYES'), 'value', 'text');
 
 		$index = (int)$this->element->attributes()->index;
 
-		$input   = array();
-		$input[] = '<fieldset id="params_audit_al' . $index . '" class="radio btn-group">';
+		/*$input   = array();
+		$input[] = '<fieldset id="params_audit_al' . $index . '" class="radio btn-group m-0">';
 
 		$value = isset($this->value->on) ? $this->value->on : $this->default;
+
+
+
 
 		foreach($options as $i => $option)
 		{
@@ -43,13 +46,29 @@ class JFormFieldCckAuditEvents extends \Joomla\CMS\Form\FormField
 
 			$required = '';
 		}
-		$input[] = '</fieldset>';
+		$input[] = '</fieldset>';*/
+
+		$inputData = [
+			'options' => $options,
+			'id' => $this->id,
+			'name' => $this->name . '[on]',
+			'class' => $this->class,
+			'onchange' => $this->onchange,
+			'dataAttribute' => '',
+			'disabled' => '',
+			'readonly' => '',
+			'value' => isset($this->value->on) ? $this->value->on : 0
+
+		];
+
+
+		$input = Joomla\CMS\Layout\LayoutHelper::render('joomla.form.field.radio.switcher',$inputData);
 
 		$value = isset($this->value->msg) && !empty($this->value->msg) ? $this->value->msg : 'CAUDLOG' . $index;
 
-		$msg = '<input style="width:100%" class="input-medium" type="text" value="' . $value . '" name="' . $this->name . '[msg]">';
+		$msg = '<input class="form-control form-control-sm" type="text" value="' . $value . '" name="' . $this->name . '[msg]">';
 
-		$patern = '<div class="row"><div class="col-md-4">%s</div><div class="col-md-7">%s</div></div>';
-		return sprintf($patern, implode('', $input), $msg);
+		$patern = '<style>fieldset[id*="params_audit"]{margin: 0px !important;padding: 0px !important}fieldset[id*="params_audit"] .switcher{width: 8rem;}</style><div class="d-flex align-items-center"><div>%s</div><div>%s</div></div>';
+		return sprintf($patern, $input, $msg);
 	}
 }
