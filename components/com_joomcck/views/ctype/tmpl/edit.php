e7a27bf451435b5eed2ce321969970143610a467
ANDI Amine <andi.amine@gmail.com>
2024-03-16 14:08:00 +0000

    Improved - Columns now use BS5 [Edit type form]
    (cherry picked from commit 7ef3c9fafd6cf68415e75d46ebb89c8f8fb6d5dc)

--- components/com_joomcck/views/ctype/tmpl/edit.php
+++ components/com_joomcck/views/ctype/tmpl/edit.php
@@ -42,91 +42,108 @@ $wa->useScript('keepalive')
 	    <?php echo HTMLHelper::_('uitab.startTabSet', 'myTab', ['active' => 'page-main', 'recall' => true, 'breakpoint' => 768]); ?>
 
 	    <?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'page-main', \Joomla\CMS\Language\Text::_('FS_FORM')); ?>
-        <div class="float-start" style="max-width: 600px; margin-right: 20px;">
-            <div class="control-group">
-                <div class="control-label"><?php echo $this->form->getLabel('id'); ?></div>
-                <div class="controls"><?php echo $this->form->getInput('id'); ?></div>
-            </div>
-            <div class="control-group">
-                <div class="control-label"><?php echo $this->form->getLabel('name'); ?></div>
-                <div class="controls"><?php echo $this->form->getInput('name'); ?></div>
-            </div>
-            <div class="control-group">
-                <div class="control-label"><?php echo $this->form->getLabel('published'); ?></div>
-                <div class="controls"><?php echo $this->form->getInput('published'); ?></div>
-            </div>
-            <div class="control-group">
-                <div class="control-label"><?php echo $this->form->getLabel('language'); ?></div>
-                <div class="controls"><?php echo $this->form->getInput('language'); ?></div>
-            </div>
-		    <?php echo $this->form->getLabel('description'); ?>
-		    <?php echo $this->form->getInput('description'); ?>
+                <div class="row">
+                    <div class="col-md-8">
+                        <div class="control-group">
+                            <div class="control-label"><?php echo $this->form->getLabel('id'); ?></div>
+                            <div class="controls"><?php echo $this->form->getInput('id'); ?></div>
+                        </div>
+                        <div class="control-group">
+                            <div class="control-label"><?php echo $this->form->getLabel('name'); ?></div>
+                            <div class="controls"><?php echo $this->form->getInput('name'); ?></div>
+                        </div>
+                        <div class="control-group">
+                            <div class="control-label"><?php echo $this->form->getLabel('published'); ?></div>
+                            <div class="controls"><?php echo $this->form->getInput('published'); ?></div>
+                        </div>
+                        <div class="control-group">
+                            <div class="control-label"><?php echo $this->form->getLabel('language'); ?></div>
+                            <div class="controls"><?php echo $this->form->getInput('language'); ?></div>
+                        </div>
+		                <?php echo $this->form->getLabel('description'); ?>
+		                <?php echo $this->form->getInput('description'); ?>
 
-        </div>
-        <div class="float-start" style="max-width: 600px;">
-		    <?php echo MFormHelper::renderFieldset($this->params_form, 'prop', $this->item->params, 'properties'); ?>
-        </div>
+                    </div>
+                    <div class="col-md-4">
+		                <?php echo MFormHelper::renderFieldset($this->params_form, 'prop', $this->item->params, 'properties'); ?>
+                    </div>
+                </div>
 	    <?php echo HTMLHelper::_('uitab.endTab'); ?>
 	    <?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'page-params', \Joomla\CMS\Language\Text::_('FS_GENERAL')); ?>
-        <div class="float-start" style="max-width: 600px; margin-right: 20px;">
-		    <?php echo MFormHelper::renderFieldset($this->params_form, 'templates', $this->item->params, 'properties'); ?>
-		    <?php echo MFormHelper::renderFieldset($this->params_form, 'tags', $this->item->params, 'properties'); ?>
-        </div>
-        <div class="float-start" style="max-width: 600px;">
-		    <?php echo MFormHelper::renderFieldset($this->params_form, 'title', $this->item->params, 'properties'); ?>
-		    <?php echo MFormHelper::renderFieldset($this->params_form, 'rating', $this->item->params, 'properties'); ?>
-        </div>
+                <div class="row">
+                    <div class="col-md-8">
+		                <?php echo MFormHelper::renderFieldset($this->params_form, 'templates', $this->item->params, 'properties'); ?>
+		                <?php echo MFormHelper::renderFieldset($this->params_form, 'tags', $this->item->params, 'properties'); ?>
+                    </div>
+                    <div class="col-md-4">
+		                <?php echo MFormHelper::renderFieldset($this->params_form, 'title', $this->item->params, 'properties'); ?>
+		                <?php echo MFormHelper::renderFieldset($this->params_form, 'rating', $this->item->params, 'properties'); ?>
+                    </div>
+                </div>
 	    <?php echo HTMLHelper::_('uitab.endTab'); ?>
 	    <?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'page-submission', \Joomla\CMS\Language\Text::_('FS_SUBMISPARAMS')); ?>
-        <div class="float-start" style="max-width: 600px; margin-right: 20px;">
-		    <?php echo MFormHelper::renderFieldset($this->params_form, 'submit', $this->item->params, 'submission'); ?>
-        </div>
-        <div class="float-start" style="max-width: 600px;">
-		    <?php echo MFormHelper::renderFieldset($this->params_form, 'categories', $this->item->params, 'submission'); ?>
-		    <?php echo MFormHelper::renderFieldset($this->params_form, 'metadata', $this->item->params, 'submission'); ?>
-        </div>
+            <div class="row">
+                <div class="col-md-8">
+                    <?php echo MFormHelper::renderFieldset($this->params_form, 'submit', $this->item->params, 'submission'); ?>
+                </div>
+                <div class="col-md-4">
+                    <?php echo MFormHelper::renderFieldset($this->params_form, 'categories', $this->item->params, 'submission'); ?>
+                    <?php echo MFormHelper::renderFieldset($this->params_form, 'metadata', $this->item->params, 'submission'); ?>
+                </div>
+            </div>
 	    <?php echo HTMLHelper::_('uitab.endTab'); ?>
 	    <?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'page-limit', \Joomla\CMS\Language\Text::_('CCATEGORYLIMIT')); ?>
-        <div class="float-start" style="max-width: 600px; margin-right: 20px;">
-		    <?php echo MFormHelper::renderGroup($this->params_form, $this->item->params, 'category_limit'); ?>
-        </div>
-        <div class="float-start">
-            <legend><?php echo \Joomla\CMS\Language\Text::_('CCATEGORYLIMIT') ?></legend>
-		    <?php echo \Joomla\CMS\HTML\HTMLHelper::_('mrelements.catselector', 'params[category_limit][category][]', 0, @$this->item->params['category_limit']['category'], 0); ?>
-        </div>
+            <div class="row">
+                <div class="col-md-8">
+		            <?php echo MFormHelper::renderGroup($this->params_form, $this->item->params, 'category_limit'); ?>
+                </div>
+                <div class="col-md-4">
+                    <legend><?php echo \Joomla\CMS\Language\Text::_('CCATEGORYLIMIT') ?></legend>
+		            <?php echo \Joomla\CMS\HTML\HTMLHelper::_('mrelements.catselector', 'params[category_limit][category][]', 0, @$this->item->params['category_limit']['category'], 0); ?>
+                </div>
+            </div>
 	    <?php echo HTMLHelper::_('uitab.endTab'); ?>
 	    <?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'page-comments', \Joomla\CMS\Language\Text::_('FS_COMMPARAMS')); ?>
-        <div class="float-start" style="max-width: 600px; margin-right: 20px;">
-		    <?php echo MFormHelper::renderGroup($this->params_form, $this->item->params, 'comments'); ?>
-            <div id="comments-params"></div>
-        </div>
+
+		    <div class="row">
+                <div class="col-md-8">
+	                <?php echo MFormHelper::renderGroup($this->params_form, $this->item->params, 'comments'); ?>
+                    <div id="comments-params"></div>
+                </div>
+            </div>
+
 	    <?php echo HTMLHelper::_('uitab.endTab'); ?>
 	    <?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'page-audit', \Joomla\CMS\Language\Text::_('FS_AUDIT')); ?>
-        <div class="float-start" style="max-width: 600px; margin-right: 20px;">
-		    <?php echo MFormHelper::renderFieldset($this->params_form, 'ver', $this->item->params, 'audit'); ?>
-		    <?php echo MFormHelper::renderFieldset($this->params_form, 'verl', $this->item->params, 'audit'); ?>
-		    <?php echo MFormHelper::renderFieldset($this->params_form, 'verw', $this->item->params, 'audit'); ?>
-        </div>
-        <div class="float-start" style="max-width: 600px;">
-		    <?php echo MFormHelper::renderFieldset($this->params_form, 'ver2', $this->item->params, 'audit'); ?>
-        </div>
+                <div class="row">
+                    <div class="col-md-8">
+		                <?php echo MFormHelper::renderFieldset($this->params_form, 'ver', $this->item->params, 'audit'); ?>
+		                <?php echo MFormHelper::renderFieldset($this->params_form, 'verl', $this->item->params, 'audit'); ?>
+		                <?php echo MFormHelper::renderFieldset($this->params_form, 'verw', $this->item->params, 'audit'); ?>
+                    </div>
+                    <div class="col-md-12">
+	                    <?php echo MFormHelper::renderFieldset($this->params_form, 'ver2', $this->item->params, 'audit'); ?>
+                    </div>
+                </div>
 	    <?php echo HTMLHelper::_('uitab.endTab'); ?>
 	    <?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'page-emerald', \Joomla\CMS\Language\Text::_('FS_EMERALD')); ?>
         <p class="lead"><?php echo \Joomla\CMS\Language\Text::_('FS_EMERALDINTEGRATE') ?></p>
 
-        <div class="float-start" style="max-width: 600px; margin-right: 20px;">
-		    <?php echo MFormHelper::renderFieldset($this->params_form, 'type_subscr', $this->item->params, 'emerald'); ?>
-		    <?php echo MFormHelper::renderFieldset($this->params_form, 'type_subscr6', $this->item->params, 'emerald'); ?>
-		    <?php echo MFormHelper::renderFieldset($this->params_form, 'type_subscr13', $this->item->params, 'emerald'); ?>
-		    <?php echo MFormHelper::renderFieldset($this->params_form, 'type_subscr1', $this->item->params, 'emerald'); ?>
+        <div class="row">
+            <div class="col-md-6">
+		        <?php echo MFormHelper::renderFieldset($this->params_form, 'type_subscr', $this->item->params, 'emerald'); ?>
+		        <?php echo MFormHelper::renderFieldset($this->params_form, 'type_subscr6', $this->item->params, 'emerald'); ?>
+		        <?php echo MFormHelper::renderFieldset($this->params_form, 'type_subscr13', $this->item->params, 'emerald'); ?>
+		        <?php echo MFormHelper::renderFieldset($this->params_form, 'type_subscr1', $this->item->params, 'emerald'); ?>
+            </div>
+            <div class="col-md-6">
+		        <?php echo MFormHelper::renderFieldset($this->params_form, 'type_subscr2', $this->item->params, 'emerald'); ?>
+		        <?php echo MFormHelper::renderFieldset($this->params_form, 'type_subscr3', $this->item->params, 'emerald'); ?>
+		        <?php echo MFormHelper::renderFieldset($this->params_form, 'type_subscr12', $this->item->params, 'emerald'); ?>
+		        <?php echo MFormHelper::renderFieldset($this->params_form, 'type_subscr4', $this->item->params, 'emerald'); ?>
+		        <?php echo MFormHelper::renderFieldset($this->params_form, 'type_subscr5', $this->item->params, 'emerald'); ?>
+            </div>
         </div>
-        <div class="float-start" style="max-width: 600px;">
-		    <?php echo MFormHelper::renderFieldset($this->params_form, 'type_subscr2', $this->item->params, 'emerald'); ?>
-		    <?php echo MFormHelper::renderFieldset($this->params_form, 'type_subscr3', $this->item->params, 'emerald'); ?>
-		    <?php echo MFormHelper::renderFieldset($this->params_form, 'type_subscr12', $this->item->params, 'emerald'); ?>
-		    <?php echo MFormHelper::renderFieldset($this->params_form, 'type_subscr4', $this->item->params, 'emerald'); ?>
-		    <?php echo MFormHelper::renderFieldset($this->params_form, 'type_subscr5', $this->item->params, 'emerald'); ?>
-        </div>
+
 	    <?php echo HTMLHelper::_('uitab.endTab'); ?>
 
         <?php echo HTMLHelper::_('uitab.endTabSet'); ?>
@@ -137,27 +154,24 @@ $wa->useScript('keepalive')
     </form>
 </div>
 
-<script type="text/javascript">
-	<!--
-	!function($) {
-		function loadCommentParams() {
-			$.ajax({
-				url: '<?php echo \Joomla\CMS\Router\Route::_('index.php?option=com_joomcck&task=ajax.loadcommentparams&tmpl=component');?>',
-				context: $('#comments-params'),
-				dataType: 'html',
-				data: {
-					adp: $('#params_comments_comments').val(),
-					type:<?php echo \Joomla\CMS\Factory::getApplication()->input->getInt('id',0)?>
-				}
-			}).done(function(data) {
-				$(this).html(data);
-				Joomcck.redrawBS();
-			});
-		}
+<script>
+    jQuery(document).ready(function($){
+        function loadCommentParams() {
+            $.ajax({
+                url: '<?php echo \Joomla\CMS\Router\Route::_('index.php?option=com_joomcck&task=ajax.loadcommentparams&tmpl=component');?>',
+                context: $('#comments-params'),
+                dataType: 'html',
+                data: {
+                    adp: $('#params_comments_comments').val(),
+                    type:<?php echo \Joomla\CMS\Factory::getApplication()->input->getInt('id',0)?>
+                }
+            }).done(function(data) {
+                $(this).html(data);
+                Joomcck.redrawBS();
+            });
+        }
 
-		loadCommentParams();
-		$('#params_comments_comments').change(loadCommentParams);
-
-	}(window.jQuery)
-	//-->
+        loadCommentParams();
+        $('#params_comments_comments').change(loadCommentParams);
+    });
 </script>
