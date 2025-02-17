93ca3cf77c7afaa8b46d534c649f7d3ba2a50b23
ANDI <andi.amine@gmail.com>
2025-02-17 08:50:57 +0100

    Improved - added start icon to feature/unfeature button
    (cherry picked from commit 702771e495d4c779ad0d008ecc7755aac47fc5b5)

--- components/com_joomcck/views/items/tmpl/default.php
+++ components/com_joomcck/views/items/tmpl/default.php
@@ -9,6 +9,7 @@
 
 use Joomcck\Html\Helpers\Dropdown;
 use Joomcck\Layout\Helpers\Layout;
+use Joomla\CMS\Language\Text;
 use Joomla\CMS\Uri\Uri;
 
 defined('_JEXEC') or die('Restricted access');
@@ -31,7 +32,7 @@ $listDirn = $this->state->get('list.direction');
 <div class="page-header">
     <h1>
         <img src="<?php echo Uri::root(TRUE); ?>/components/com_joomcck/images/icons/items.png">
-		<?php echo \Joomla\CMS\Language\Text::_('XML_TOOLBAR_TITLE_RECORDS'); ?>
+		<?php echo Text::_('XML_TOOLBAR_TITLE_RECORDS'); ?>
     </h1>
 </div>
 
@@ -60,7 +61,7 @@ $listDirn = $this->state->get('list.direction');
             <table class="table table-hover" id="articleList">
                 <thead>
                 <th width="1%">
-                    <input type="checkbox" name="checkall-toggle" value="" title="<?php echo \Joomla\CMS\Language\Text::_('JGLOBAL_CHECK_ALL'); ?>" onclick="Joomla.checkAll(this)"/>
+                    <input type="checkbox" name="checkall-toggle" value="" title="<?php echo Text::_('JGLOBAL_CHECK_ALL'); ?>" onclick="Joomla.checkAll(this)"/>
                 </th>
                 <th class="nowrap">
 			        <?php echo \Joomla\CMS\HTML\HTMLHelper::_('grid.sort', 'JSTATUS', 'a.published', $listDirn, $listOrder); ?>
@@ -88,11 +89,11 @@ $listDirn = $this->state->get('list.direction');
                         </td>
                         <td class="center">
                             <div class="btn-group">
-                                <a class="btn btn-sm btn-light border" rel="tooltip" data-bs-toggle="tooltip" title="<?php echo $item->published ? \Joomla\CMS\Language\Text::_('CUNPUB') : \Joomla\CMS\Language\Text::_('CPUB'); ?>"
+                                <a class="btn btn-sm btn-light border" rel="tooltip" data-bs-toggle="tooltip" title="<?php echo $item->published ? Text::_('CUNPUB') : Text::_('CPUB'); ?>"
                                    href="<?php echo Url::task('records.' . ($item->published ? 'sunpub' : 'spub'), $item->id); ?>">
 							        <?php echo HTMLFormatHelper::icon(!$item->published ? 'cross-circle.png' : 'tick.png'); ?>
                                 </a>
-                                <a class="btn btn-sm btn-light border" rel="tooltip" data-bs-toggle="tooltip" title="<?php echo $item->featured ? \Joomla\CMS\Language\Text::_('CMAKEUNFEATURE') : \Joomla\CMS\Language\Text::_('CMAKEFEATURE'); ?>"
+                                <a class="btn btn-sm btn-light border" rel="tooltip" data-bs-toggle="tooltip" title="<?php echo $item->featured ? Text::_('CMAKEUNFEATURE') : Text::_('CMAKEFEATURE'); ?>"
                                    href="<?php echo Url::task('records.' . ($item->featured ? 'sunfeatured' : 'sfeatured'), $item->id); ?>">
 							        <?php echo HTMLFormatHelper::icon(!$item->featured ? 'crown-silver.png' : 'crown.png'); ?>
                                 </a>
@@ -106,7 +107,7 @@ $listDirn = $this->state->get('list.direction');
                                 <div>
                                     <small>
 								        <?php echo \Joomla\CMS\HTML\HTMLHelper::_('ip.country', $item->ip); ?>
-								        <?php echo \Joomla\CMS\HTML\HTMLHelper::link('javascript:void(0);', $item->ip, array('rel' => "tooltip", 'title' => \Joomla\CMS\Language\Text::_('CFILTERBYIP'), 'onclick' => 'Joomcck.setAndSubmit(\'filter_search\', \'ip:' . $item->ip . '\');')); ?>
+								        <?php echo \Joomla\CMS\HTML\HTMLHelper::link('javascript:void(0);', $item->ip, array('rel' => "tooltip", 'title' => Text::_('CFILTERBYIP'), 'onclick' => 'Joomcck.setAndSubmit(\'filter_search\', \'ip:' . $item->ip . '\');')); ?>
                                     </small>
                                 </div>
 					        <?php endif; ?>
@@ -115,9 +116,9 @@ $listDirn = $this->state->get('list.direction');
                             <div style="position: absolute; top: 10px; right: 10px;">
 						        <?php
 						        // Create dropdown items
-						        Dropdown::addCustomItem('<i class="fas fa-copy"></i> '.\Joomla\CMS\Language\Text::_('CCOPY'), 'javascript:void(0)', 'onclick="Joomla.listItemTask(\'cb' . $i . '\',\'records.copy\')"');
-						        Dropdown::addCustomItem($item->featured ? \Joomla\CMS\Language\Text::_('CMAKEUNFEATURE') : \Joomla\CMS\Language\Text::_('CMAKEFEATURED'), Url::task('records.' . ($item->featured ? 'sunfeatured' : 'sfeatured'), $item->id));
-						        Dropdown::addCustomItem('<i class="fas fa-trash text-danger"></i> '. \Joomla\CMS\Language\Text::_('CDELETE'), Url::task('records.delete', $item->id));
+						        Dropdown::addCustomItem('<i class="fas fa-copy"></i> '. Text::_('CCOPY'), 'javascript:void(0)', 'onclick="Joomla.listItemTask(\'cb' . $i . '\',\'records.copy\')"');
+						        Dropdown::addCustomItem($item->featured ? '<i class="fas fa-star text-muted"></i> '. Text::_('CMAKEUNFEATURE') : '<i class="fas fa-star text-warning"></i> '. Text::_('CMAKEFEATURED'), Url::task('records.' . ($item->featured ? 'sunfeatured' : 'sfeatured'), $item->id));
+						        Dropdown::addCustomItem('<i class="fas fa-trash text-danger"></i> '. Text::_('CDELETE'), Url::task('records.delete', $item->id));
 
 						        Dropdown::divider();
 
@@ -134,23 +135,23 @@ $listDirn = $this->state->get('list.direction');
 
 						        Dropdown::divider();
 
-						        Dropdown::addCustomItem( \Joomla\CMS\Language\Text::_('C_TOOLBAR_RESET_CTIME'), 'javascript:void(0)', 'onclick="Joomla.Joomla.listItemTask(\'cb' . $i . '\',\'records.reset_ctime\')"');
-						        Dropdown::addCustomItem( \Joomla\CMS\Language\Text::_('C_TOOLBAR_RESET_MTIME'), 'javascript:void(0)', 'onclick="Joomla.listItemTask(\'cb' . $i . '\',\'records.reset_mtime\')"');
-						        Dropdown::addCustomItem( \Joomla\CMS\Language\Text::_('C_TOOLBAR_RESET_EXTIME'), 'javascript:void(0)', 'onclick="Joomla.listItemTask(\'cb' . $i . '\',\'records.reset_extime\')"');
+						        Dropdown::addCustomItem( Text::_('C_TOOLBAR_RESET_CTIME'), 'javascript:void(0)', 'onclick="Joomla.Joomla.listItemTask(\'cb' . $i . '\',\'records.reset_ctime\')"');
+						        Dropdown::addCustomItem( Text::_('C_TOOLBAR_RESET_MTIME'), 'javascript:void(0)', 'onclick="Joomla.listItemTask(\'cb' . $i . '\',\'records.reset_mtime\')"');
+						        Dropdown::addCustomItem( Text::_('C_TOOLBAR_RESET_EXTIME'), 'javascript:void(0)', 'onclick="Joomla.listItemTask(\'cb' . $i . '\',\'records.reset_extime\')"');
 
 						        Dropdown::divider();
 
 						        if($item->hits):
-							        Dropdown::addCustomItem( \Joomla\CMS\Language\Text::_('C_TOOLBAR_RESET_HITS') . " <span class=\"badge bg-info\">{$item->hits}</span>", 'javascript:void(0)', 'onclick="Joomla.listItemTask(\'cb' . $i . '\',\'records.reset_hits\')"');
+							        Dropdown::addCustomItem( Text::_('C_TOOLBAR_RESET_HITS') . " <span class=\"badge bg-info\">{$item->hits}</span>", 'javascript:void(0)', 'onclick="Joomla.listItemTask(\'cb' . $i . '\',\'records.reset_hits\')"');
 						        endif;
 						        if($item->comments):
-							        Dropdown::addCustomItem( \Joomla\CMS\Language\Text::_('C_TOOLBAR_RESET_COOMENT') . " <span class=\"badge bg-info\">{$item->comments}</span>", 'javascript:void(0)', 'onclick="Joomla.listItemTask(\'cb' . $i . '\',\'records.reset_com\')"');
+							        Dropdown::addCustomItem( Text::_('C_TOOLBAR_RESET_COOMENT') . " <span class=\"badge bg-info\">{$item->comments}</span>", 'javascript:void(0)', 'onclick="Joomla.listItemTask(\'cb' . $i . '\',\'records.reset_com\')"');
 						        endif;
 						        if($item->votes):
-							        Dropdown::addCustomItem( \Joomla\CMS\Language\Text::_('C_TOOLBAR_RESET_RATING') . " <span class=\"badge bg-info\">{$item->votes}</span>", 'javascript:void(0)', 'onclick="Joomla.listItemTask(\'cb' . $i . '\',\'records.reset_vote\')"');
+							        Dropdown::addCustomItem( Text::_('C_TOOLBAR_RESET_RATING') . " <span class=\"badge bg-info\">{$item->votes}</span>", 'javascript:void(0)', 'onclick="Joomla.listItemTask(\'cb' . $i . '\',\'records.reset_vote\')"');
 						        endif;
 						        if($item->favorite_num):
-							        Dropdown::addCustomItem( \Joomla\CMS\Language\Text::_('C_TOOLBAR_RESET_FAVORIT') . " <span class=\"badge bg-info\">{$item->favorite_num}</span>", 'javascript:void(0)', 'onclick="Joomla.listItemTask(\'cb' . $i . '\',\'records.reset_fav\')"');
+							        Dropdown::addCustomItem( Text::_('C_TOOLBAR_RESET_FAVORIT') . " <span class=\"badge bg-info\">{$item->favorite_num}</span>", 'javascript:void(0)', 'onclick="Joomla.listItemTask(\'cb' . $i . '\',\'records.reset_fav\')"');
 						        endif;
 
 						        echo Dropdown::render();
@@ -160,35 +161,35 @@ $listDirn = $this->state->get('list.direction');
 						        <?php if($item->checked_out) : ?>
 							        <?php echo \Joomla\CMS\HTML\HTMLHelper::_('jgrid.checkedout', $i, $item->checked_out, $item->checked_out_time, 'records.', $canCheckin); ?>
 						        <?php endif; ?>
-                                <a title="<?php echo \Joomla\CMS\Language\Text::_('CEDITRECORD'); ?>" href="<?php echo Url::edit((int)$item->id); ?>">
+                                <a title="<?php echo Text::_('CEDITRECORD'); ?>" href="<?php echo Url::edit((int)$item->id); ?>">
                                     <big><?php echo strip_tags($item->title); ?></big>
                                 </a>
                                 <br/>
                                 <small>
-							        <?php echo \Joomla\CMS\Language\Text::_('CTYPE'); ?>:
-                                    <a href="#" rel="tooltip" data-bs-toggle="tooltip" title="<?php echo \Joomla\CMS\Language\Text::_('CFILTERBYTYPE'); ?>" onclick="Joomcck.setAndSubmit('filter_type', <?php echo $item->type_id ?>)">
+							        <?php echo Text::_('CTYPE'); ?>:
+                                    <a href="#" rel="tooltip" data-bs-toggle="tooltip" title="<?php echo Text::_('CFILTERBYTYPE'); ?>" onclick="Joomcck.setAndSubmit('filter_type', <?php echo $item->type_id ?>)">
 								        <?php echo $this->escape($item->type_name); ?>
                                     </a>
                                     <span style="color: lightgray">|</span>
 
-							        <?php echo \Joomla\CMS\Language\Text::_('CSECTION'); ?>:
-                                    <a href="#" rel="tooltip" data-bs-toggle="tooltip" title="<?php echo \Joomla\CMS\Language\Text::_('CFILTERBYSECTION'); ?>" onclick="Joomcck.setAndSubmit('filter_section', <?php echo $item->section_id ?>)">
+							        <?php echo Text::_('CSECTION'); ?>:
+                                    <a href="#" rel="tooltip" data-bs-toggle="tooltip" title="<?php echo Text::_('CFILTERBYSECTION'); ?>" onclick="Joomcck.setAndSubmit('filter_section', <?php echo $item->section_id ?>)">
 								        <?php echo $this->escape($item->section_name); ?>
                                     </a>
 
 							        <?php if($item->categories): ?>
                                         <span style="color: lightgray">|</span>
-								        <?php echo \Joomla\CMS\Language\Text::_('CCATEGORY'); ?>:
+								        <?php echo Text::_('CCATEGORY'); ?>:
 								        <?php foreach($item->categories AS $key => $category): ?>
-                                            <a rel="tooltip" data-bs-toggle="tooltip" title="<?php echo \Joomla\CMS\Language\Text::_('CFILTERBYCATEGORY'); ?>" href="#" onclick="Joomcck.setAndSubmit('filter_category', <?php echo $key; ?>);"><?php echo $category; ?></a>
+                                            <a rel="tooltip" data-bs-toggle="tooltip" title="<?php echo Text::_('CFILTERBYCATEGORY'); ?>" href="#" onclick="Joomcck.setAndSubmit('filter_category', <?php echo $key; ?>);"><?php echo $category; ?></a>
 								        <?php endforeach; ?>
 							        <?php endif; ?>
 
                                     <span style="color: lightgray">|</span>
-							        <?php echo \Joomla\CMS\Language\Text::_('CAUTHOR'); ?>:
+							        <?php echo Text::_('CAUTHOR'); ?>:
                                     <small>
-								        <?php echo \Joomla\CMS\HTML\HTMLHelper::link('javascript:void(0);', ($item->userlogin ? $item->userlogin : \Joomla\CMS\Language\Text::_('CANONYMOUS')), array(
-									        'rel' => "tooltip", 'title' => \Joomla\CMS\Language\Text::_('CFILTERBYUSER'), 'onclick' => 'Joomcck.setAndSubmit(\'filter_search\', \'user:' . $item->user_id . '\');'
+								        <?php echo \Joomla\CMS\HTML\HTMLHelper::link('javascript:void(0);', ($item->userlogin ? $item->userlogin : Text::_('CANONYMOUS')), array(
+									        'rel' => "tooltip", 'title' => Text::_('CFILTERBYUSER'), 'onclick' => 'Joomcck.setAndSubmit(\'filter_search\', \'user:' . $item->user_id . '\');'
 								        )) ?>
 
                                     </small>
@@ -199,12 +200,12 @@ $listDirn = $this->state->get('list.direction');
                                     <span class="badge text-bg-light shadow-sm px-2 py-1"><small><?php echo $this->escape($item->hits); ?></small></span>
 
 							        <?php echo \Joomla\CMS\HTML\HTMLHelper::_('grid.sort', 'CCOMMENTS', 'a.comments', $listDirn, $listOrder); ?>
-                                    <a rel="tooltip" data-bs-toggle="tooltip" title="<?php echo \Joomla\CMS\Language\Text::_('CSHOWRECORDCOMMENTS'); ?>" href="<?php echo \Joomla\CMS\Router\Route::_('index.php?option=com_joomcck&view=comms&filter_search=record:' . $item->id); ?>" class="badge bg-info">
+                                    <a rel="tooltip" data-bs-toggle="tooltip" title="<?php echo Text::_('CSHOWRECORDCOMMENTS'); ?>" href="<?php echo \Joomla\CMS\Router\Route::_('index.php?option=com_joomcck&view=comms&filter_search=record:' . $item->id); ?>" class="badge bg-info">
                                         <small><?php echo $this->escape($item->comments); ?></small>
                                     </a>
 
 							        <?php echo \Joomla\CMS\HTML\HTMLHelper::_('grid.sort', 'CVOTES', 'a.votes', $listDirn, $listOrder); ?>
-                                    <a rel="tooltip" data-bs-toggle="tooltip" title="<?php echo \Joomla\CMS\Language\Text::_('CSHOWRECORDVOTES'); ?>" href="<?php echo \Joomla\CMS\Router\Route::_('index.php?option=com_joomcck&view=votes&filter_search=record:' . $item->id); ?>" class="badge bg-info">
+                                    <a rel="tooltip" data-bs-toggle="tooltip" title="<?php echo Text::_('CSHOWRECORDVOTES'); ?>" href="<?php echo \Joomla\CMS\Router\Route::_('index.php?option=com_joomcck&view=votes&filter_search=record:' . $item->id); ?>" class="badge bg-info">
                                         <small><?php echo $this->escape($item->votes); ?></small>
                                     </a>
 
@@ -216,13 +217,13 @@ $listDirn = $this->state->get('list.direction');
                         <td nowrap="nowrap">
                             <small>
 						        <?php $data = new \Joomla\CMS\Date\Date($item->ctime);
-						        echo $data->format(\Joomla\CMS\Language\Text::_('CDATE1')); ?><br/>
+						        echo $data->format(Text::_('CDATE1')); ?><br/>
 						        <?php if($item->extime == '0000-00-00 00:00:00' || is_null($item->extime)): ?>
-                                    <span style="color: green"><?php echo \Joomla\CMS\Language\Text::_('CNEVER') ?></span>
+                                    <span style="color: green"><?php echo Text::_('CNEVER') ?></span>
 						        <?php else: ?>
 							        <?php $extime = new \Joomla\CMS\Date\Date($item->extime); ?>
                                     <span style="color: <?php echo($extime->toUnix() <= time() ? 'red' : 'green') ?>">
-							<?php echo $extime->format(\Joomla\CMS\Language\Text::_('CDATE1')); ?>
+							<?php echo $extime->format(Text::_('CDATE1')); ?>
 							</span>
 						        <?php endif; ?>
                             </small>
