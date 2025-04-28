2dc9a6bc4dae929431fa48df1bfbdd85fe7e5bdb
ANDI <andi.amine@gmail.com>
2025-04-28 21:16:51 +0100

    New Features - Ability to truncate description and also possibility to split or not words [Open Graph Field] Improved - Cleaning description method was improved [Open Graph Field]
    (cherry picked from commit 97b18097ac6b226b91155f0d9a4d7184a1818eec)

--- components/com_joomcck/fields/opengraph/opengraph.php
+++ components/com_joomcck/fields/opengraph/opengraph.php
@@ -8,7 +8,9 @@
 
 use Joomla\CMS\Component\ComponentHelper;
 use Joomla\CMS\Factory;
+use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Router\Route;
+use Joomla\CMS\HTML\Helpers\StringHelper;
 
 defined('_JEXEC') or die();
 require_once JPATH_ROOT . DIRECTORY_SEPARATOR . 'components/com_joomcck/library/php/fields/joomcckfield.php';
@@ -25,100 +27,116 @@ class JFormFieldCOpengraph extends CFormField
 	public function getInput()
 	{
 		return;
-	}	
+	}
 
 	public function onRenderFull($record, $type, $section)
 	{
 		// set fields ids
-		$this->pictureFieldId = $this->params->get('params.pic_id',0);
-		$this->descriptionFieldId = $this->params->get('params.desc_id',0);
+		$this->pictureFieldId = $this->params->get('params.pic_id', 0);
+		$this->descriptionFieldId = $this->params->get('params.desc_id', 0);
+
+		// Get description truncation settings
+		$desc_length = (int)$this->params->get('params.desc_length', 200);
+		$no_split = (bool)$this->params->get('params.no_split', 1);
 
 		// get general upload directory
 		$fupload = ComponentHelper::getParams('com_joomcck')->get('general_upload');
 
 		// get default pictures
-		$default_pic = $this->params->get('params.default_pic','');
+		$default_pic = $this->params->get('params.default_pic', '');
 		$default_pic = !empty($default_pic) ? \Joomla\CMS\HTML\HTMLHelper::cleanImageURL($default_pic)->url : '';
-
 
 		// get og type
 		$og_type = $this->params->get('params.og_type');
 
+		// Check for Facebook App ID
+		$app_id = $this->params->get('params.fb_app_id', '');
+
 		$site_name = $this->sitename();
 		$item_title = $record->title;
-		$item_link 	= Route::_($record->href);
+		$item_link = Route::_($record->href);
 		$out = '';
 		$purl = '';
 
+		// Prepare field IDs
+		$ids = [
+			'pic' => $this->pictureFieldId,
+			'desc' => $this->descriptionFieldId
+		];
 
-		
 		// get gallery field path
 		if(!empty($ids['pic'])){
 			$fparams = $this->_getFieldParamsFromDB($ids['pic']);
 			$fparams = json_decode($fparams, true);
 			$fparams = $fparams['params'];
 			$subf = $fparams['subfolder']; //subfolder value
-			
-			if(!isset($fparams['params']['select_type'])){ // is gallery field				
-								
+
+			if(!isset($fparams['params']['select_type'])){ // is gallery field
+
 				$purl = \Joomla\CMS\Uri\Uri::root().$fupload.'/'.$subf.'/';
 			}
-			
 		}
-		
-		
+
 		// get image url
-		if(!empty($ids['pic'])){ 
-			
-			$pfiles = $this->_getFieldInfo($ids['pic'], $record, true);		
-			
-			
+		if(!empty($ids['pic'])){
+
+			$pfiles = $this->_getFieldInfo($ids['pic'], $record, true);
+
 			if(!empty($pfiles)){
-				
-				if(is_array($pfiles)){ // if gallery field					
-					
+
+				if(is_array($pfiles)){ // if gallery field
+
 					foreach ($pfiles as $ppfile){
-						$item_pic_url[] = $purl.$ppfile;
-					}				
-				
+						$cleanImage = HtmlHelper::cleanImageURL($ppfile);
+						$item_pic_url[] = $purl.$cleanImage->url;
+					}
+
 				}else{ // if simple image field
-					$item_pic_url = \Joomla\CMS\Uri\Uri::root().$pfiles;
+
+					$cleanImage = HtmlHelper::cleanImageURL($pfiles);
+					$item_pic_url = \Joomla\CMS\Uri\Uri::root().$cleanImage->url;
 				}
-				
+
 			}else{
-				if(!empty($default_pic)){ // default image						
-					$item_pic_url = \Joomla\CMS\Uri\Uri::root().$default_pic;
+				if(!empty($default_pic)){ // default image
+
+					$cleanImage = HtmlHelper::cleanImageURL($default_pic);
+
+					$item_pic_url = \Joomla\CMS\Uri\Uri::root().$cleanImage->url;
 				}else
 					$item_pic_url = '';
-			}			
-			
-			
-		}	
+			}
+
+		}
 		elseif(!empty($default_pic)){ // default image
-			
-			$item_pic_url = \Joomla\CMS\Uri\Uri::root().$default_pic;
-		}	
+
+			$cleanImage = HtmlHelper::cleanImageURL($default_pic);
+			$item_pic_url = \Joomla\CMS\Uri\Uri::root().$cleanImage->url;
+		}
 		else{ // nothing found
-			
+
 			$item_pic_url = '';
-		}			
-		
+		}
+
 		// get description
-		if(!empty($ids['desc'])) // field content
-			$item_desc = $this->_getFieldInfo($ids['desc'], $record);
-		elseif(!empty($record->meta_descr)) // record meta desc
-			$item_desc = $record->meta_descr;
-		else // website global desc
-			$item_desc = $this->sitedesc();
-		
+		if(!empty($ids['desc'])) { // field content
+			$item_desc = $this->_getFieldInfo($ids['desc'], $record, false, $desc_length, $no_split);
+		}
+		elseif(!empty($record->meta_descr)) { // record meta desc
+			$item_desc = $this->truncateDescription($record->meta_descr, $desc_length, $no_split);
+		}
+		else { // website global desc
+			$item_desc = $this->truncateDescription($this->sitedesc(), $desc_length, $no_split);
+		}
+
 		// build open graph tags
-		$out .= '<meta property="og:title" content="'.$item_title.'">';
-		$out .= '<meta property="og:site_name" content="'.$site_name.'">';
+		$out .= '<meta property="og:title" content="'.$this->cleanString($item_title).'">';
+		$out .= '<meta property="og:site_name" content="'.$this->cleanString($site_name).'">';
 		$out .= '<meta property="og:url" content="'.$item_link.'">';
 
 		if(!empty($item_desc))
-		$out .= '<meta property="og:description" content="'.$item_desc.'">';		
-		
+			$out .= '<meta property="og:description" content="'.$item_desc.'">';
+
 		if(!empty($item_pic_url)){
 			if(is_array($item_pic_url)){
 				foreach ($item_pic_url as $ipu){ // for gallery field
@@ -126,28 +144,66 @@ class JFormFieldCOpengraph extends CFormField
 				}
 			}
 			else // for simple image field
-				$out .= '<meta property="og:image" content="'.$item_pic_url.'" />';	
+				$out .= '<meta property="og:image" content="'.$item_pic_url.'" />';
 		}
+
 		if(!empty($app_id))
-		$out .= '<meta property="fb:app_id" content="'.$app_id.'">';
-		
+			$out .= '<meta property="fb:app_id" content="'.$app_id.'">';
+
 		$out .= '<meta property="og:type" content="'.$og_type.'">';
-		
+
 		// add open graph tags in head
 		Factory::getDocument()->addCustomtag($out);
-		
 	}
 
-	public function setGalleryFieldPath(){
-		
+	/**
+	 * Truncate description and add ellipsis if needed
+	 *
+	 * @param string $text The description text to truncate
+	 * @param int $length Maximum length of description
+	 * @param bool $noSplit Whether to avoid splitting words
+	 * @return string Truncated description
+	 */
+	private function truncateDescription($text, $length = 200, $noSplit = true)
+	{
+		// clear first
+		$text = $this->cleanString($text);
+
+		// return truncated
+		return \Joomla\CMS\HTML\Helpers\StringHelper::truncate($text, $length, $noSplit, false);
+	}
+
+	/**
+	 * Clean string for Open Graph meta tags
+	 * Removes extra whitespace and special characters
+	 *
+	 * @param string $string Text to clean
+	 * @return string Cleaned string
+	 */
+	private function cleanString($string)
+	{
+
+		// Strip BBCode first
+		$string = $this->_stripBBCode($string);
+
+		// Strip tags
+		$string = strip_tags($string);
+
+		// Remove line breaks and extra spaces
+		$string = preg_replace('/\s+/', ' ', $string);
+
+		// Escape quotes
+		$string = htmlspecialchars($string, ENT_QUOTES, 'UTF-8');
+
+		return trim($string);
 	}
 
 	public function onRenderList($record, $type, $section)
-	{		
+	{
 		return;
 	}
 
-		public function onImport($value, $params, $record = null)
+	public function onImport($value, $params, $record = null)
 	{
 		return;
 	}
@@ -155,53 +211,62 @@ class JFormFieldCOpengraph extends CFormField
 	public function onImportForm($heads, $defaults)
 	{
 		return $this->_import_fieldlist($heads, $defaults->get('field.' . $this->id));
-	}	
+	}
 
-	private function _getFieldInfo($id, $record, $isPIC = null){
-		$fields =json_decode($record->fields, true);
+	private function _getFieldInfo($id, $record, $isPIC = false, $desc_length = 200, $noSplit = true)
+	{
+		$fields = json_decode($record->fields, true);
+
 		if($isPIC)
 		{
 			if(isset($fields[$id]['image']))
 				$pic = $fields[$id]['image'];
-				
-			elseif($fields[$id]){
+
+			elseif(isset($fields[$id]) && is_array($fields[$id])){
 				$pic = array();
 				foreach ($fields[$id] as $v){
-					
-					$pic[] = $v['fullpath'];
+					if(isset($v['fullpath'])) {
+						$pic[] = $v['fullpath'];
+					}
 				}
-				
+			} else {
+				$pic = '';
 			}
-			
+
 			return $pic;
 		}
-		else 
+		else
 		{
-			$desc = substr(strip_tags($fields[$id]), 0, 300);
-			// strip bbcodes
-			$desc = $this->_stripBBCode($desc);
-			return $desc;
+			if(!isset($fields[$id])) {
+				return '';
+			}
+
+			// Use Joomla's StringHelper for truncation
+			return $this->truncateDescription($fields[$id], $desc_length, $noSplit);
 		}
 	}
-	
-	private function sitename(){
+
+	private function sitename()
+	{
 		return Factory::getApplication()->getCfg('sitename');
 	}
-	
-	private function sitedesc(){
+
+	private function sitedesc()
+	{
 		return Factory::getDocument()->getMetaData('description');
 	}
 
-	
 	// strip bbcode in desc field
-	private function _stripBBCode($desc) {
-    	$pattern = '|[[\/\!]*?[^\[\]]*?]|si';
-    	$replace = '';
-    	return preg_replace($pattern, $replace, $desc);
-    }	
-	
+	private function _stripBBCode($desc)
+	{
+		$pattern = '|[[\/\!]*?[^\[\]]*?]|si';
+		$replace = '';
+		return preg_replace($pattern, $replace, $desc);
+	}
+
 	// return field params of given id
-	private function _getFieldParamsFromDB($id){		
+	private function _getFieldParamsFromDB($id)
+	{
 		$db = Factory::getDbo();
 		$query = $db->getQuery(true);
 		$query->select('params');
@@ -210,23 +275,22 @@ class JFormFieldCOpengraph extends CFormField
 
 		$db->setQuery($query);
 		$params = $db->loadResult();
-		
+
 		return $params;
 	}
-	
+
 	// return field id of given field key
-	private function _getFieldIdFromDB($key){			
+	private function _getFieldIdFromDB($key)
+	{
 		$db = Factory::getDbo();
 		$query = $db->getQuery(true);
 		$query->select('id');
 		$query->from($db->quoteName('#__js_res_fields'));
 		$query->where($db->quoteName('key')." = ".$db->quote($key));
-	
+
 		$db->setQuery($query);
 		$id = $db->loadResult();
-	
-		return $id;
-	
-	}
 
+		return $id;
+	}
 }
