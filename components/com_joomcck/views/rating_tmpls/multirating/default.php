b706c0a05d4cf57b00f6f61abba1442896a2b4d7
ANDI Amine <andi.amine@gmail.com>
2026-04-12 20:16:46 +0100

    feat(multirating): replace table with BS5 list-group for responsive layout
    (cherry picked from commit 60740bea418d3d334d6f03baedb50cfbf372ee0c)

--- components/com_joomcck/views/rating_tmpls/multirating/default.php
+++ components/com_joomcck/views/rating_tmpls/multirating/default.php
@@ -9,35 +9,33 @@
 
 defined('_JEXEC') or die();
 
-
 ?>
-<table class="table-rating table table-hover table-bordered table-condensed">
-	<thead>
-		<tr>
-			<th width="1%" nowrap="nowrap" style="vertical-align: top;"><?php echo \Joomla\CMS\Language\Text::_('CTOTALRATING'); ?></th>
-			<td width="1%" nowrap="nowrap" valign="top">
-				<?php echo RatingHelp::loadRating($type->params->get('properties.tmpl_rating'), round(@$record->votes_result), $record->id, 500, 'Joomcck.ItemRatingCallBack', false, $record->id);?>
-				<small id="rating-text-<?php echo $record->id;?>"><?php echo \Joomla\CMS\Language\Text::sprintf('CRAINGDATA', $record->votes_result, $record->votes); ?></small>
-			</td>
-		</tr>
-	</thead>
-	<tbody>
-		<?php foreach($options as $key => $option): ?>
-			<?php
+<div class="joomcck-multirating list-group mb-3">
+    <div class="list-group-item list-group-item-primary d-flex flex-column flex-md-row justify-content-between align-items-md-center gap-2">
+        <div class="fw-bold"><?php echo \Joomla\CMS\Language\Text::_('CTOTALRATING'); ?></div>
+        <div class="text-md-end">
+            <?php echo RatingHelp::loadRating($type->params->get('properties.tmpl_rating'), round(@$record->votes_result), $record->id, 500, 'Joomcck.ItemRatingCallBack', false, $record->id); ?>
+            <small id="rating-text-<?php echo $record->id; ?>" class="d-block d-md-inline ms-md-2 text-muted"><?php echo \Joomla\CMS\Language\Text::sprintf('CRAINGDATA', $record->votes_result, $record->votes); ?></small>
+        </div>
+    </div>
 
-
-            $parts = explode('::', $option);
-			$canRate = self::canRate('record', $record->user_id, $record->id, $type->params->get('properties.rate_access'), $key, $type->params->get('properties.rate_access_author', 0));
-
-            ?>
-			<tr>
-				<th width="1%" nowrap="nowrap" style="vertical-align: top;"><?php echo \Joomla\CMS\Language\Text::_($parts[0]); ?></th>
-				<td width="1%" nowrap="nowrap" valign="top">
-					<?php echo RatingHelp::loadRating(isset($parts[1]) ? $parts[1] : $type->params->get('properties.tmpl_rating'),
-						round((int)@$result[$key]['sum']), $record->id, $key, 'Joomcck.ItemRatingCallBackMulti',
-						$canRate);?>
-				</td>
-			</tr>
-		<?php endforeach;?>
-	</tbody>
-</table>
+    <?php foreach ($options as $key => $option): ?>
+        <?php
+        $parts   = explode('::', $option);
+        $canRate = self::canRate('record', $record->user_id, $record->id, $type->params->get('properties.rate_access'), $key, $type->params->get('properties.rate_access_author', 0));
+        ?>
+        <div class="list-group-item d-flex flex-column flex-md-row justify-content-between align-items-md-center gap-2">
+            <div class="fw-semibold"><?php echo \Joomla\CMS\Language\Text::_($parts[0]); ?></div>
+            <div class="text-md-end">
+                <?php echo RatingHelp::loadRating(
+                    isset($parts[1]) ? $parts[1] : $type->params->get('properties.tmpl_rating'),
+                    round((int) @$result[$key]['sum']),
+                    $record->id,
+                    $key,
+                    'Joomcck.ItemRatingCallBackMulti',
+                    $canRate
+                ); ?>
+            </div>
+        </div>
+    <?php endforeach; ?>
+</div>
